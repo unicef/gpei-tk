7ac5f2642dccf5a95e4161ffbf090889f5f5da00
Ersan <smokecode@gmail.com>
2016-06-09 00:00:54 -0400

    add checklist relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :c4d_articles
   has_many :sop_articles
+  has_one :sop_checklist
+  has_one :c4d_checklist
   belongs_to :office
   belongs_to :role
 end
