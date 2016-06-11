5e465708a5d0a52d662702fe0b31fa74fcd014d9
Ersan <smokecode@gmail.com>
2016-06-11 15:30:06 -0400

    c4d article show route set up

--- app/controllers/c4d_articles_controller.rb
+++ app/controllers/c4d_articles_controller.rb
@@ -1,7 +1,11 @@
 class C4dArticlesController < ApplicationController
   def index
-    redirect_to c4d_path
+    redirect_to c4d_selection_path
   end
+
   def show
+    @img_name = 'C4D'
+    @c4d_article = C4dArticle.where(title: params['title'])
+    @c4d_article_last_index = C4dArticle.last.id
   end
 end
