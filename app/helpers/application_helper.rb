940466a726566174108e6b3b6912ea70012f20e5
Ersan <smokecode@gmail.com>
2016-06-11 22:09:39 -0400

    helper for visibility setting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,22 @@ module ApplicationHelper
   def build_classes args
     return args.values.join(' ')
   end
+
+  def style_visible icon, user, article
+    if icon == 'add'
+      user.sop_checklist.sop_articles.each do |check_list_article|
+        if check_list_article.title == article.title
+          return 'visibility:hidden'
+        end
+      end
+      return 'visibility:visible'
+    else
+      user.sop_checklist.sop_articles.each do |check_list_article|
+        if check_list_article.title == article.title
+          return 'visibility:visible'
+        end
+      end
+      return 'visibility:hidden'
+    end
+  end
 end
