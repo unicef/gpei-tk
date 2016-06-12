7620c58164f8bb0e74a1d35353f759c438249fde
Ersan <smokecode@gmail.com>
2016-06-11 22:05:32 -0400

    js for restful calls

--- app/assets/javascripts/sop_isotope.js
+++ app/assets/javascripts/sop_isotope.js
@@ -4,7 +4,7 @@ $(function(){
   });
 
   // clear-all filter listener
-  var $filter_clear = $('#filter_clear_all');
+  var $filter_clear = $('#sop_filter_clear_all');
   var $checkboxes = $('.sop_checkboxes input');
   $filter_clear.click(function(){
     for (var idx = 0; idx < $checkboxes.length; idx++)
@@ -29,4 +29,68 @@ $(function(){
     filterValue = filterValue === '*' ? '' : filterValue
     $output.html("<li id=\"checklist_article\">" + filterValue + "</li>");
   }
+  function toggleVisibility(el) {
+    if ($(el).css('visibility') == 'hidden' )
+      $(el).css('visibility','visible');
+    else
+      $(el).css('visibility','hidden');
+  }
+  var $add = $('.grid_add')
+
+  $add.click(function(e){
+
+    var article_title = e.currentTarget.parentElement.querySelector('#grid_item_article_title').innerHTML;
+    $.ajax({
+      method: 'POST',
+      url: '/sop/checklist/' + article_title
+    }).done(function(response) {
+      toggleVisibility(e.currentTarget);
+
+      var grid_check = e.currentTarget.nextElementSibling
+
+      toggleVisibility(e.currentTarget.nextElementSibling);
+      var article_id = $('#sop_article_id').text()
+      var list_item = "<li id=\"" + article_title + "\"><a href=\"sop_articles/" + article_id + "\">" + article_title + "</a><i class=\"fa fa-remove\" aria-hidden=\"true\"></i></li>";
+      $('#sop_checklist_list').append(list_item);
+    });
+  });
+
+  var $remove = $('.grid_check')
+  $remove.click(function(e){
+
+    var article_title = e.currentTarget.parentElement.querySelector('#grid_item_article_title').innerHTML;
+    $.ajax({
+      method: 'DELETE',
+      url: '/sop/checklist/' + article_title
+    }).done(function(response) {
+      toggleVisibility(e.currentTarget);
+
+      var grid_check = e.currentTarget.previousElementSibling
+
+      toggleVisibility(e.currentTarget.previousElementSibling);
+
+      var article_list_item = '#sop_checklist_list #'+article_title;
+      $(article_list_item).remove();
+    });
+  });
+
+  $('#sop_checklist_list').on('click', 'i', function(e) {
+    var parent_element = e.currentTarget.parentElement
+    var article_title = parent_element.id
+    debugger
+    $.ajax({
+      method: 'DELETE',
+      url: '/sop/checklist/' + article_title
+    }).done(function(response) {
+      var $grid_tile = $('#grid_tile_'+article_title)
+      var $check_icon = $('#grid_tile_'+ article_title + ' .grid_add')
+      var $add_icon = $('#grid_tile_'+ article_title + ' .grid_check')
+      toggleVisibility($check_icon);
+      toggleVisibility($add_icon);
+
+      var sop_checklist_list_item = '#sop_checklist_list #' + parent_element.id
+      debugger
+      $(sop_checklist_list_item).remove()
+    })
+  });
 });
--- app/controllers/sop_checklist_controller.rb
+++ app/controllers/sop_checklist_controller.rb
@@ -1,20 +1,27 @@
 class SopChecklistController < ApplicationController
-  def update
+  def create
     if request.xhr?
       user = User.all.first
       article = SopArticle.where(title: params['title']).first
-      if (user.sop_checklist.sop_articles.where(title: params['title']))
-        SopchecklistSoparticle.first_or_create(sop_article_id: article.id, sop_checklist_id: user.sop_checklist.id)
+      if user.sop_checklist.sop_articles.where(title: params['title']).empty?
+        user.sop_checklist.sop_articles << article
+        render json: { status: "success" }
       else
-        join_table_article = SopchecklistSoparticle.where(title: article.title, sop_checklist_id: user.sop_checklist.id).first
-        SopchecklistSoparticle.destroy(join_table_article.id)
+        render json: { status: "fail" }
       end
-      @user = user
-      @sop_times = SopTime.all
-      @sop_categories = SopCategory.all
-      @offices = Office.all
-      @sop_articles = SopArticle.all
-      render 'sop/index', layout: false
+    end
+  end
+
+  def destroy
+    if request.xhr?
+      user = User.all.first
+      article = user.sop_checklist.sop_articles.where(title: params['title']).first
+      if article
+        SopchecklistSoparticle.where(sop_article_id: article.id, sop_checklist_id: user.sop_checklist.id).first.destroy
+        render json: { status: "success" }
+      else
+        render json: { status: "fail" }
+      end
     end
   end
 end
