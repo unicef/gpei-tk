4ebc7f8b3b63ec098f2e82348c408dcea115edd4
Ersan <smokecode@gmail.com>
2016-06-12 21:59:00 -0400

    relations for reference and template links

--- app/models/c4d_article.rb
+++ app/models/c4d_article.rb
@@ -2,7 +2,8 @@ class C4dArticle < ActiveRecord::Base
   belongs_to :c4d_category
   belongs_to :c4d_subcategory
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
-
+  has_many :c4d_reference_links
+  has_many :c4d_template_links
   validates_uniqueness_of :title, :CMS_title
 
   def to_param
--- app/models/sop_article.rb
+++ app/models/sop_article.rb
@@ -4,7 +4,9 @@ class SopArticle < ActiveRecord::Base
   belongs_to :responsibility, class_name: 'Office', foreign_key: 'responsibility_id'
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   has_many :sopchecklist_soparticles
-
+  has_one :sop_icon
+  has_many :sop_reference_links
+  has_many :sop_template_links
   validates_uniqueness_of :title, :CMS_title
 
   def to_param
