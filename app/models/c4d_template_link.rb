6ca35307ee1781d151909a1037036f42b44affa7
Ersan <smokecode@gmail.com>
2016-06-13 00:02:33 -0400

    c4d template links fixed

--- app/models/c4d_template_link.rb
+++ app/models/c4d_template_link.rb
@@ -1,4 +1,5 @@
 class C4dTemplateLink < ActiveRecord::Base
   has_many :c4d_articles
+  has_many :c4d_article_c4d_template_links
   has_many :c4d_articles, :through =>:c4d_article_c4d_template_links
 end
