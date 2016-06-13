b515042833a0ccbebd24357c3c62e3e7783f16b6
Ersan <smokecode@gmail.com>
2016-06-12 21:59:51 -0400

    related links for sop and c4d articles

--- app/controllers/sop_articles_controller.rb
+++ app/controllers/sop_articles_controller.rb
@@ -11,5 +11,12 @@ class SopArticlesController < ApplicationController
     else
       @sop_article = SopArticle.where(id: params['id']).first
     end
+    if !SopArticle.last.nil?
+      @sop_article_last_index = SopArticle.last.id
+    else
+      @sop_article_last_index = 0
+    end
+    @sop_category_articles = SopArticle.where(sop_category: @sop_article.sop_category_id)
+    @sop_related_topics = SopArticle.where(sop_category: @sop_article.sop_category, sop_time: @sop_article.sop_time)
   end
 end
