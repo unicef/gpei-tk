78087d65ff11dab06a646f2171c063830f38d1c0
Ersan <smokecode@gmail.com>
2016-06-13 23:42:49 -0400

    helper method for show views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,39 @@ module ApplicationHelper
       return 'visibility:hidden'
     end
   end
+  def c4d_style_visible icon, user, article
+    if icon == 'add'
+      user.c4d_toolkit.c4d_articles.each do |check_list_article|
+        if check_list_article.title == article.title
+          return 'visibility:hidden'
+        end
+      end
+      return 'visibility:visible'
+    else
+      user.c4d_toolkit.c4d_articles.each do |check_list_article|
+        if check_list_article.title == article.title
+          return 'visibility:visible'
+        end
+      end
+      return 'visibility:hidden'
+    end
+  end
+
+  def get_color_for_subcategory article_subcategory
+    special_categories = ['ID Your Scenario', 'Monitor Evaluate', 'Innovations']
+    if special_categories.include?(article_subcategory)
+      return 'black'
+    else
+      return 'white'
+    end
+  end
+
+  def get_color_for_sop_time article_time
+    special_categories = ['14 Days-Close']
+    if special_categories.include?(article_time)
+      return 'black'
+    else
+      return 'white'
+    end
+  end
 end
