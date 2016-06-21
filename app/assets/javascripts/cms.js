268f0789761fea3e17f1e6bee1caf21c2ef4387e
eUstundag <smokecode@gmail.com>
2016-06-21 14:21:05 -0400

    js for user load and update role

--- app/assets/javascripts/cms.js
+++ app/assets/javascripts/cms.js
@@ -5,12 +5,87 @@ $(function(){
       trigger: '.title .icon'
     }
   });
-  $('#CMS_index_content').on('click', '#CMS_user_assign_role', function(e){
-    
+
+  $('#CMS_index_content').on('click', '#CMS_user_assign_role', e => {
+    $('.ui.modal')
+      .modal({
+        onHide: () => {
+          $('.ui.modal .content').empty()
+          $('.ui.modal .header').empty()
+        }
+      })
+      .modal('toggle')
+      // .modal('show');
+
+    $('.ui.modal .header').append('Modify User Role')
+    $.ajax({
+      method: 'GET',
+      url: 'roles/'
+    }).done(response => {
+      let element_id = '#CMS_index_content tr#' + e.currentTarget.parentElement.id + ' td'
+      let user_element = $(element_id)
+      // first name last name email role
+      let content = getUserForm(user_element, response.roles, e.currentTarget.parentElement.id)
+      $('.ui.modal .content').append(content)
+      $('.ui.radio.checkbox')
+      .checkbox();
+      modifyRoleSubmit()
+    })
   })
-  $('#CMS_index_content').on('click', '#CMS_user_remove_membership', function(e){
+  function modifyRoleSubmit(){
+    $(document).find('.ui.modal').submit((e) => {
+      e.preventDefault();
+      $.ajax({
+        method: 'PATCH',
+        url: 'cms/users/' + $(e.currentTarget).find('form').attr('id'),
+        data: $(e.currentTarget).find('.ui.form').serialize()
+      }).done(response => {
+        $('.ui.modal').modal('hide')
+        $('#CMS_users_table').find('#'+response.id).find('td')[$('#CMS_users_table').find('#'+response.id).find('td').length-2].innerHTML = response.role.title
+      })
+    })
+  }
+  function getUserForm(user_element, roles, id) {
+    return (
+    `<form id="${id}" class="ui form">
+      <div class="disabled field">
+        <label>First Name</label>
+        <input type="text" name="first_name" placeholder="${user_element[0].innerHTML}" value="${user_element[0].innerHTML}">
+      </div>
+      <div class="disabled field">
+        <label>Last Name</label>
+        <input type="text" name="last_name" placeholder="${user_element[1].innerHTML}" value="${user_element[1].innerHTML}">
+      </div>
+      <div class="disabled field">
+        <label>Email</label>
+        <input type="text" name="email" placeholder="${user_element[2].innerHTML}" value="${user_element[2].innerHTML}">
+      </div>
+      ${getRolesDropdown(roles, user_element[3].innerHTML)}
+      <button class="ui button" type="submit">Submit</button>
+    </form>`)
+  }
+  function getRolesDropdown(roles, user_role) {
+    return (
+      `<div class="inline fields">
+        <label for="role">Select user role:</label>
+        ${_.map(roles, role => {
+            let checked = ''
+            if (user_role === role.title) { checked = 'checked' }
+            return `<div class="field">
+              <div class="ui radio checkbox">
+                <input type="radio" name="role_id" checked="${checked}" tabindex="0" class="hidden" value="${role.id}">
+                <label>${role.title}</label>
+              </div>
+            </div>`
+          }).join('')}
+      </div>`
+    )
+  }
+
+  $('#CMS_index_content').on('click', '#CMS_user_remove_membership', e => {
 
   })
+
   $('#CMS_users_link').click(e => {
     e.preventDefault();
 
@@ -33,9 +108,6 @@ $(function(){
       });
     })
   })
-  function getUserHeader(){
-
-  }
   function getUserActionDropdown(id){
     return (
       '<div class="ui buttons"><div id="CMS_actions_dropdown" class="ui button">Actions</div><div class="ui floating dropdown icon button"><i class="dropdown icon"></i><div class="menu"><div id="' + id + '" class="item"><span id="CMS_user_assign_role">Assign Roles</span></div><div id="' + id + '" class="item"><span id="CMS_user_remove_membership">Remove Membership</span></div></div></div>'
