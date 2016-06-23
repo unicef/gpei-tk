98e5c204229e56ddad3554f237e1d73effddd366
eUstundag <smokecode@gmail.com>
2016-06-23 13:03:13 -0400

    cms sop article controller

---
+++ app/controllers/cms/sop_articles_controller.rb
@@ -0,0 +1,36 @@
+class Cms::SopArticlesController < ApplicationController
+  def index
+    if request.xhr?
+      sop_articles = SopArticle.all
+      users = {}
+      User.all.each do |user|
+        users[user.id] = user
+      end
+      sop_articles.each do |article|
+        article.owner_id = User.all.first.id
+      end
+      render json: { sop_articles: sop_articles, users: users, status: 'success' }
+    end
+  end
+
+  def show
+    if request.xhr?
+      sop_article = SopArticle.find(params[:id])
+      sop_times = SopTime.all
+      sop_categories = SopCategory.all
+      render json: { sop_article: sop_article, sop_times: sop_times, sop_categories: sop_categories, status: 'success' }
+    end
+  end
+
+  def update
+    if request.xhr?
+      article = SopArticle.find(params[:id])
+      article.update(safe_update_params)
+      render json: { status: 'success' }
+    end
+  end
+
+  def safe_update_params
+    params.permit(:cms_title, :title, :responsible, :support, :article, :video_url, :sop_time_id, :sop_category_id)
+  end
+end
