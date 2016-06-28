d383ac2fad95de2992ee6c7a07361a0c942761ac
eUstundag <smokecode@gmail.com>
2016-06-28 13:28:21 -0400

    update soparticle show js

--- app/assets/javascripts/sop_article_show.js
+++ app/assets/javascripts/sop_article_show.js
@@ -1,36 +1,67 @@
-// sop_checklist_icons
-
-  var $sopcheck_list_icons = $('#sop_checklist_icons')
-  $sopcheck_list_icons.on('click', '#grid_add', function(e){
-    var article_title = e.currentTarget.parentElement.querySelector('#grid_item_article_title').innerHTML;
+$(function(){
+  let $sopcheck_list_icons = $('#sop_article_logo_row')
+  $sopcheck_list_icons.on('click', '.grid_add', function(e){
+    let article_title = e.currentTarget.parentElement.querySelector('#grid_item_article_title').innerHTML;
+    let id = e.currentTarget.id
+    debugger
     $.ajax({
       method: 'POST',
-      url: '/sop/checklist/' + article_title
+      url: '/sop/checklist/',
+      data: { title: article_title, id: id }
     }).done(function(response) {
       toggleVisibility(e.currentTarget);
 
-      var grid_check = e.currentTarget.nextElementSibling
+      let grid_check = e.currentTarget.nextElementSibling
 
       toggleVisibility(e.currentTarget.nextElementSibling);
-      var article_id = $('#sop_article_id').text()
-      var list_item = "<li id=\"" + article_title + "\"><a href=\"sop_articles/" + article_id + "\">" + article_title + "</a><i class=\"fa fa-remove\" aria-hidden=\"true\"></i></li>";
+      let article_title = response.article_title
+      let id = response.id
+      let list_item = "<div id=\"" + id + "\" class=\"item\" style='background-color: black;color: white;'><a href=sop_articles/" + id + " style='background-color: black;color: white;'>" + article_title + "</a> <i id=\"checklist_item" + article_title + "\" class=\"fa fa-remove\" aria-hidden=\"true\" style='background-color: black;color: white;'></i></div>"
+      removeNoArticlesSelected('#sop_no_items_selected')
       $('#sop_checklist_list').append(list_item);
     });
   });
 
-  $sopcheck_list_icons.on('click', '#grid_check', function(e){
-    var article_title = e.currentTarget.parentElement.querySelector('#grid_item_article_title').innerHTML;
+  $sopcheck_list_icons.on('click', '.grid_check', function(e){
+    let article_title = e.currentTarget.parentElement.querySelector('#grid_item_article_title').innerHTML;
+    let id = e.currentTarget.id
+    debugger
     $.ajax({
       method: 'DELETE',
-      url: '/sop/checklist/' + article_title
+      url: '/sop/checklist/',
+      data: { title: article_title, id: id }
     }).done(function(response) {
+      debugger
       toggleVisibility(e.currentTarget);
 
-      var grid_check = e.currentTarget.previousElementSibling
+      let grid_check = e.currentTarget.previousElementSibling
 
       toggleVisibility(e.currentTarget.previousElementSibling);
 
-      var article_list_item = '#sop_checklist_list #'+article_title;
+      let article_list_item = '#sop_checklist_list #'+response.id;
       $(article_list_item).remove();
+      checkIfArticlesSelectedAndAppend('#sop_checklist_list')
     });
   });
+
+  function toggleVisibility(el) {
+    if ($(el).css('visibility') == 'hidden' )
+      $(el).css('visibility','visible');
+    else
+      $(el).css('visibility','hidden');
+  }
+
+  function checkIfArticlesSelectedAndAppend(el_id){
+    var items = el_id + " .item"
+    if ($(items).length === 0) {
+      var list_id = el_id
+      var no_items = "<div class=\"item\" id=\"sop_no_items_selected\"><span style='background-color: black;color: white;'>No articles selected</span></div>"
+      $(list_id).append(no_items)
+    }
+  }
+  function removeNoArticlesSelected(ele) {
+    var el = "#sop_checklist_list " + ele
+    $(el).remove()
+  }
+
+})
