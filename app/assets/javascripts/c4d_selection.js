7d43cb7ea2d2a8032b4baed96266623c639e5c64
eUstundag <smokecode@gmail.com>
2016-06-28 16:29:36 -0400

    update targets for c4d selection

--- app/assets/javascripts/c4d_selection.js
+++ app/assets/javascripts/c4d_selection.js
@@ -1,33 +1,33 @@
 $(function(){
-  var $grid = $('#c4d_category_grid').isotope({
+  let $grid = $('#c4d_category_grid').isotope({
     itemSelector: '.c4d_grid_item'
   });
 // // filter functions
-//   var filterFns = {
+//   let filterFns = {
 //     // show if number is greater than 50
 //     numberGreaterThan50: function() {
-//       var number = $(this).find('.number').text();
+//       let number = $(this).find('.number').text();
 //       return parseInt( number, 10 ) > 50;
 //     },
 //     // show if name ends with -ium
 //     ium: function() {
-//       var name = $(this).find('.name').text();
+//       let name = $(this).find('.name').text();
 //       return name.match( /ium$/ );
 //     }
 //   };
 // bind filter button click
 $('.c4d-filters-button-group').on('click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
+  let filterValue = $(this).attr('data-filter');
   // use filterFn if matches value
   // filterValue = filterFns[ filterValue ] || filterValue;
   $grid.isotope({ filter: filterValue });
 });
 // change is_checked class on buttons
 $('.button-group').each(function(i, buttonGroup) {
-  var $buttonGroup = $(buttonGroup);
+  let $buttonGroup = $(buttonGroup);
   $buttonGroup.on( 'click', 'button', function() {
     $buttonGroup.find('.is_checked').removeClass('is_checked');
-    // var color_class = getColorClass($(this).attr('class').substr(7,$(this).attr('class').length))
+    // let color_class = getColorClass($(this).attr('class').substr(7,$(this).attr('class').length))
     $(this).addClass('is_checked');
   });
   // function getColorClass(class_string) {
@@ -56,27 +56,27 @@ $('.button-group').each(function(i, buttonGroup) {
 // })
 
 //   // clear-all filter listener
-//   var $filter_clear = $('#sop_filter_clear_all');
-//   var $checkboxes = $('.sop_checkboxes input');
+//   let $filter_clear = $('#sop_filter_clear_all');
+//   let $checkboxes = $('.sop_checkboxes input');
 //   $filter_clear.click(function(){
-//     for (var idx = 0; idx < $checkboxes.length; idx++)
+//     for (let idx = 0; idx < $checkboxes.length; idx++)
 //       $checkboxes[idx].checked = false;
 //     updateIsotope();
 //   });
 
 //   //isotope listener
-//   var $output = $('#sop_filter');
+//   let $output = $('#sop_filter');
 //   $checkboxes.change(function() {
 //     updateIsotope();
 //   });
 
 //   function updateIsotope(){
-//     var inclusives = [];
+//     let inclusives = [];
 //     $checkboxes.each( function( i, elem ) {
 //       if ( elem.checked )
 //         inclusives.push( elem.value );
 //     });
-//     var filterValue = inclusives.length ? inclusives.join(', ') : '*';
+//     let filterValue = inclusives.length ? inclusives.join(', ') : '*';
 //     $container.isotope({ filter: filterValue })
 //     filterValue = filterValue === '*' ? '' : filterValue
 //     $output.html("<li id=\"checklist_article\">" + filterValue + "</li>");
@@ -87,73 +87,76 @@ $('.button-group').each(function(i, buttonGroup) {
     else
       $(el).css('visibility','hidden');
   }
-  var $add = $('.c4d_grid_add')
+  let $add = $('#c4d_category_grid .c4d_grid_add')
 
   $add.click(function(e){
-    var article_title = e.currentTarget.parentElement.querySelector('.c4d_grid_item_article_title').innerHTML;
+    let article_title = e.currentTarget.parentElement.querySelector('.c4d_grid_item_article_title').innerHTML;
+    let article_id = e.currentTarget.parentElement.id
     $.ajax({
       method: 'POST',
-      url: '/c4d/toolkit/' + article_title
+      url: '/c4d/toolkit/',
+      data: { title: article_title, id: article_id }
     }).done(function(response) {
       toggleVisibility(e.currentTarget);
 
-      var grid_check = e.currentTarget.nextElementSibling
+      let grid_check = e.currentTarget.nextElementSibling
 
       toggleVisibility(e.currentTarget.nextElementSibling);
-      var article_id = $('#c4d_article_id').text()
-      var list_item = "<div id=\"" + article_title + "\" class=\"item\" style='background-color: black;color: white;'><a href=/c4d_articles/" + article_id + " style='background-color: black;color: white;'>" + article_title + "</a> <i id=\"" + article_title + "\" class=\"fa fa-remove\" aria-hidden=\"true\" style='background-color: black;color: white;'></i></div>"
+      let article_id = response.id
+      let list_item = "<div id=\"" + article_id + "\" class=\"item\" style='background-color: black;color: white;'><a href=/c4d_articles/" + article_id + " style='background-color: black;color: white;'>" + article_title + "</a> <i id=\"" + article_title + "\" class=\"fa fa-remove\" aria-hidden=\"true\" style='background-color: black;color: white;'></i></div>"
       removeNoArticlesSelected('#c4d_no_items_selected')
       $('#c4d_toolkit_list').append(list_item);
     });
   });
   function removeNoArticlesSelected(ele) {
-    var el = "#c4d_toolkit_list " + ele
+    let el = "#c4d_toolkit_list " + ele
     $(el).remove()
   }
 
-  var $remove = $('.c4d_grid_check')
+  let $remove = $('#c4d_category_grid .c4d_grid_check')
   $remove.click(function(e){
-
-    var article_title = e.currentTarget.parentElement.querySelector('.c4d_grid_item_article_title').innerHTML;
+    let article_title = e.currentTarget.parentElement.querySelector('.c4d_grid_item_article_title').innerHTML;
+    let article_id = e.currentTarget.parentElement.id
     $.ajax({
       method: 'DELETE',
-      url: '/sop/checklist/' + article_title
+      url: '/c4d/toolkit/',
+      data: { title: article_title, id: article_id }
     }).done(function(response) {
       toggleVisibility(e.currentTarget);
 
-      var grid_check = e.currentTarget.previousElementSibling
+      let grid_check = e.currentTarget.previousElementSibling
 
       toggleVisibility(e.currentTarget.previousElementSibling);
-      var article_list_item = '#c4d_toolkit_list #'+article_title;
+      let article_list_item = '#c4d_toolkit_list #'+response.id;
       $(article_list_item).remove();
       checkIfArticlesSelectedAndAppend('#c4d_toolkit_list')
     });
   });
 
   $('#c4d_toolkit_list').on('click', 'i', function(e) {
-    var article_title = e.currentTarget.id
-    var parent_element = e.currentTarget.parentElement
+    let article_title = e.currentTarget.id
+    let parent_element = e.currentTarget.parentElement
     $.ajax({
       method: 'DELETE',
-      url: '/c4d/toolkit/' + article_title
+      url: '/c4d/toolkit/',
+      data: { title: article_title, id: parent_element.id }
     }).done(function(response) {
-      var $grid_tile = $('#c4d_grid_tile_'+article_title)
-      var $check_icon = $grid_tile.find('.c4d_grid_check')
-      var $add_icon = $grid_tile.find('.c4d_grid_add')
-
+      let $grid_tile = $('#c4d_category_grid #'+ parent_element.id)
+      let $check_icon = $grid_tile.find('.c4d_grid_check')
+      let $add_icon = $grid_tile.find('.c4d_grid_add')
       toggleVisibility($check_icon);
       toggleVisibility($add_icon);
-      var c4d_toolkit_list_item = '#c4d_toolkit_list #' + parent_element.id
+      let c4d_toolkit_list_item = '#c4d_toolkit_list #' + parent_element.id
       $(c4d_toolkit_list_item).remove()
       checkIfArticlesSelectedAndAppend('#c4d_toolkit_list')
     })
   });
 
   function checkIfArticlesSelectedAndAppend(el_id){
-    var items = el_id + " .item"
+    let items = el_id + " .item"
     if ($(items).length === 0) {
-      var list_id = el_id
-      var no_items = "<div class=\"item\" id=\"c4d_no_items_selected\"><span style='background-color: black;color: white;'>No articles selected</span></div>"
+      let list_id = el_id
+      let no_items = "<div class=\"item\" id=\"c4d_no_items_selected\"><span style='background-color: black;color: white;'>No articles selected</span></div>"
       $(list_id).append(no_items)
     }
   }
