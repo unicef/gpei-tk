9e806e90e40123381d50140207c5c55ca65395b4
eUstundag <smokecode@gmail.com>
2016-06-28 16:29:52 -0400

    update routes for c4d toolkit

--- app/controllers/c4d_toolkit_controller.rb
+++ app/controllers/c4d_toolkit_controller.rb
@@ -3,9 +3,9 @@ class C4dToolkitController < ApplicationController
     if request.xhr?
       user = User.all.first
       article = C4dArticle.find_by(id: params['id'])
-      if user.c4d_toolkit.c4d_articles.find_by(title: article.title).nil?
+      if user.c4d_toolkit.c4d_articles.find_by(id: article.id).nil?
         user.c4d_toolkit.c4d_articles << article
-        render json: { status: "success" }
+        render json: { status: "success", id: article.id, title: article.title }
       else
         render json: { status: "fail" }
       end
@@ -15,10 +15,10 @@ class C4dToolkitController < ApplicationController
   def destroy
     if request.xhr?
       user = User.all.first
-      article = user.c4d_toolkit.c4d_articles.find_by(title: params['title'])
+      article = user.c4d_toolkit.c4d_articles.find_by(id: params['id'])
       if article
         C4dToolkitC4dArticle.where(c4d_article_id: article.id, c4d_toolkit_id: user.c4d_toolkit.id).first.destroy
-        render json: { status: "success" }
+        render json: { status: "success", id: params['id'] }
       else
         render json: { status: "fail" }
       end
