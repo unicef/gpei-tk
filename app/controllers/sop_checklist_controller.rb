2d8ac5de9aa7fd203de56948ee906949531e7009
eUstundag <smokecode@gmail.com>
2016-06-28 13:35:30 -0400

    queries no longer break runtime and return needed data in response

--- app/controllers/sop_checklist_controller.rb
+++ app/controllers/sop_checklist_controller.rb
@@ -2,10 +2,10 @@ class SopChecklistController < ApplicationController
   def create
     if request.xhr?
       user = User.all.first
-      article = SopArticle.where(title: params['title']).first
-      if user.sop_checklist.sop_articles.where(title: params['title']).empty?
+      article = SopArticle.find_by(id: params['id'])
+      if user.sop_checklist.sop_articles.find_by(title: article.title).nil?
         user.sop_checklist.sop_articles << article
-        render json: { status: "success" }
+        render json: { status: "success", id: article.id, article_title: article.title }
       else
         render json: { status: "fail" }
       end
@@ -15,10 +15,10 @@ class SopChecklistController < ApplicationController
   def destroy
     if request.xhr?
       user = User.all.first
-      article = user.sop_checklist.sop_articles.where(title: params['title']).first
+      article = user.sop_checklist.sop_articles.find_by(id: params['id'])
       if article
         SopChecklistSopArticle.where(sop_article_id: article.id, sop_checklist_id: user.sop_checklist.id).first.destroy
-        render json: { status: "success" }
+        render json: { status: "success", id: article.id, article_title: article.title }
       else
         render json: { status: "fail" }
       end
