f077773013479340b6995ee3f8dcd7fa7de71af7
eUstundag <smokecode@gmail.com>
2016-06-28 13:45:27 -0400

    update queries to not break runtime

--- app/controllers/c4d_toolkit_controller.rb
+++ app/controllers/c4d_toolkit_controller.rb
@@ -2,8 +2,8 @@ class C4dToolkitController < ApplicationController
   def create
     if request.xhr?
       user = User.all.first
-      article = C4dArticle.where(title: params['title']).first
-      if user.c4d_toolkit.c4d_articles.where(title: params['title']).empty?
+      article = C4dArticle.find_by(id: params['id'])
+      if user.c4d_toolkit.c4d_articles.find_by(title: article.title).nil?
         user.c4d_toolkit.c4d_articles << article
         render json: { status: "success" }
       else
@@ -15,7 +15,7 @@ class C4dToolkitController < ApplicationController
   def destroy
     if request.xhr?
       user = User.all.first
-      article = user.c4d_toolkit.c4d_articles.where(title: params['title']).first
+      article = user.c4d_toolkit.c4d_articles.find_by(title: params['title'])
       if article
         C4dToolkitC4dArticle.where(c4d_article_id: article.id, c4d_toolkit_id: user.c4d_toolkit.id).first.destroy
         render json: { status: "success" }
