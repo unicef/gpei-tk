7e85c0c846c10d8e6471bf35fefce493a5d04602
eUstundag <smokecode@gmail.com>
2016-07-07 15:44:13 -0400

    clean up c4d js

--- app/assets/javascripts/c4d_selection.js
+++ app/assets/javascripts/c4d_selection.js
@@ -2,24 +2,8 @@ $(() => {
   let $grid = $('#c4d_category_grid').isotope({
     itemSelector: '.c4d_grid_item'
   })
-// // filter functions
-//   let filterFns = {
-//     // show if number is greater than 50
-//     numberGreaterThan50: function() {
-//       let number = $(this).find('.number').text()
-//       return parseInt( number, 10 ) > 50
-//     },
-//     // show if name ends with -ium
-//     ium: function() {
-//       let name = $(this).find('.name').text()
-//       return name.match( /ium$/ )
-//     }
-//   }
-// bind filter button click
 $('.c4d-filters-button-group').on('click', 'button', e => {
   let filterValue = $(e.currentTarget).attr('data-filter')
-  // use filterFn if matches value
-  // filterValue = filterFns[ filterValue ] || filterValue
   $grid.isotope({ filter: filterValue })
 })
 // change is_checked class on buttons
@@ -44,117 +28,87 @@ function getColorClass(category_text) {
   else if (category_text === 'Tools')
     return '#009DA4'
 }
-// $('.button-group').on('click', '.c4d_selection_background_Act', function(){
-//   $(this).toggleClass('Act_color')
-// })
-// $('.button-group').on('click', '.c4d_selection_background_Plan', function(){
-//   $(this).toggleClass('Plan_color')
-// })
-// $('.button-group').on('click', '.c4d_selection_background_Understand', function(){
-//   $(this).toggleClass('Understand_color')
-// })
-// $('.button-group').on('click', '.c4d_selection_background_Tools', function(){
-//   $(this).toggleClass('Tools_color')
-// })
+function toggleVisibility(el) {
+  if ($(el).css('visibility') == 'hidden' )
+    $(el).css('visibility','visible')
+  else
+    $(el).css('visibility','hidden')
+}
+let $add = $('#c4d_category_grid .c4d_grid_add')
 
-//   //isotope listener
-//   let $output = $('#sop_filter')
-//   $checkboxes.change(function() {
-//     updateIsotope()
-//   })
+$add.click(e => {
+  e.preventDefault()
+  let article_title = e.currentTarget.parentElement.querySelector('.c4d_grid_item_article_title').innerHTML
+  let article_id = e.currentTarget.parentElement.id
+  $.ajax({
+    method: 'POST',
+    url: '/c4d/toolkit/',
+    data: { title: article_title, id: article_id }
+  }).done(response => {
+    toggleVisibility(e.currentTarget)
 
-//   function updateIsotope(){
-//     let inclusives = []
-//     $checkboxes.each( function( i, elem ) {
-//       if ( elem.checked )
-//         inclusives.push( elem.value )
-//     })
-//     let filterValue = inclusives.length ? inclusives.join(', ') : '*'
-//     $container.isotope({ filter: filterValue })
-//     filterValue = filterValue === '*' ? '' : filterValue
-//     $output.html("<li id=\"checklist_article\">" + filterValue + "</li>")
-//   }
-  function toggleVisibility(el) {
-    if ($(el).css('visibility') == 'hidden' )
-      $(el).css('visibility','visible')
-    else
-      $(el).css('visibility','hidden')
-  }
-  let $add = $('#c4d_category_grid .c4d_grid_add')
+    let grid_check = e.currentTarget.nextElementSibling
 
-  $add.click(e => {
-    e.preventDefault()
-    let article_title = e.currentTarget.parentElement.querySelector('.c4d_grid_item_article_title').innerHTML
-    let article_id = e.currentTarget.parentElement.id
-    $.ajax({
-      method: 'POST',
-      url: '/c4d/toolkit/',
-      data: { title: article_title, id: article_id }
-    }).done(response => {
-      toggleVisibility(e.currentTarget)
-
-      let grid_check = e.currentTarget.nextElementSibling
-
-      toggleVisibility(e.currentTarget.nextElementSibling)
-      let article_id = response.id
-      let list_item = "<div id=\"" + article_id + "\" class=\"item\"><a class=\"white_text_black_bg\" href=\"/c4d_articles/" + article_id + "\">" + article_title + "</a> <i id=\"" + article_title + "\" class=\"fa fa-remove white_text_black_bg\" aria-hidden=\"true\"></i></div>"
-      removeNoArticlesSelected('#c4d_no_items_selected')
-      $('#c4d_toolkit_list').append(list_item)
-    })
+    toggleVisibility(e.currentTarget.nextElementSibling)
+    let article_id = response.id
+    let list_item = "<div id=\"" + article_id + "\" class=\"item\"><a class=\"white_text_black_bg\" href=\"/c4d_articles/" + article_id + "\">" + article_title + "</a> <i id=\"" + article_title + "\" class=\"fa fa-remove white_text_black_bg\" aria-hidden=\"true\"></i></div>"
+    removeNoArticlesSelected('#c4d_no_items_selected')
+    $('#c4d_toolkit_list').append(list_item)
   })
-  function removeNoArticlesSelected(ele) {
-    let el = "#c4d_toolkit_list " + ele
-    $(el).remove()
-  }
+})
+function removeNoArticlesSelected(ele) {
+  let el = "#c4d_toolkit_list " + ele
+  $(el).remove()
+}
 
-  let $remove = $('#c4d_category_grid .c4d_grid_check')
-  $remove.click(e => {
-    e.preventDefault()
-    let article_title = e.currentTarget.parentElement.querySelector('.c4d_grid_item_article_title').innerHTML
-    let article_id = e.currentTarget.parentElement.id
-    $.ajax({
-      method: 'DELETE',
-      url: '/c4d/toolkit/',
-      data: { title: article_title, id: article_id }
-    }).done(response => {
-      toggleVisibility(e.currentTarget)
+let $remove = $('#c4d_category_grid .c4d_grid_check')
+$remove.click(e => {
+  e.preventDefault()
+  let article_title = e.currentTarget.parentElement.querySelector('.c4d_grid_item_article_title').innerHTML
+  let article_id = e.currentTarget.parentElement.id
+  $.ajax({
+    method: 'DELETE',
+    url: '/c4d/toolkit/',
+    data: { title: article_title, id: article_id }
+  }).done(response => {
+    toggleVisibility(e.currentTarget)
 
-      let grid_check = e.currentTarget.previousElementSibling
+    let grid_check = e.currentTarget.previousElementSibling
 
-      toggleVisibility(e.currentTarget.previousElementSibling)
-      let article_list_item = '#c4d_toolkit_list #'+response.id
-      $(article_list_item).remove()
-      checkIfArticlesSelectedAndAppend('#c4d_toolkit_list')
-    })
+    toggleVisibility(e.currentTarget.previousElementSibling)
+    let article_list_item = '#c4d_toolkit_list #'+response.id
+    $(article_list_item).remove()
+    checkIfArticlesSelectedAndAppend('#c4d_toolkit_list')
   })
+})
 
-  $('#c4d_toolkit_list').on('click', 'i', e => {
-    e.preventDefault()
-    let article_title = e.currentTarget.id
-    let parent_element = e.currentTarget.parentElement
-    $.ajax({
-      method: 'DELETE',
-      url: '/c4d/toolkit/',
-      data: { title: article_title, id: parent_element.id }
-    }).done(response => {
-      let $grid_tile = $('#c4d_category_grid #'+ parent_element.id)
-      let $check_icon = $grid_tile.find('.c4d_grid_check')
-      let $add_icon = $grid_tile.find('.c4d_grid_add')
-      toggleVisibility($check_icon)
-      toggleVisibility($add_icon)
-      let c4d_toolkit_list_item = '#c4d_toolkit_list #' + parent_element.id
-      $(c4d_toolkit_list_item).remove()
-      checkIfArticlesSelectedAndAppend('#c4d_toolkit_list')
-    })
+$('#c4d_toolkit_list').on('click', 'i', e => {
+  e.preventDefault()
+  let article_title = e.currentTarget.id
+  let parent_element = e.currentTarget.parentElement
+  $.ajax({
+    method: 'DELETE',
+    url: '/c4d/toolkit/',
+    data: { title: article_title, id: parent_element.id }
+  }).done(response => {
+    let $grid_tile = $('#c4d_category_grid #'+ parent_element.id)
+    let $check_icon = $grid_tile.find('.c4d_grid_check')
+    let $add_icon = $grid_tile.find('.c4d_grid_add')
+    toggleVisibility($check_icon)
+    toggleVisibility($add_icon)
+    let c4d_toolkit_list_item = '#c4d_toolkit_list #' + parent_element.id
+    $(c4d_toolkit_list_item).remove()
+    checkIfArticlesSelectedAndAppend('#c4d_toolkit_list')
   })
+})
 
-  function checkIfArticlesSelectedAndAppend(el_id){
-    let items = el_id + " .item"
-    if ($(items).length === 0) {
-      let list_id = el_id
-      let no_items = "<div id=\"c4d_no_items_selected\" class=\"item white_text_black_bg\">No articles selected</div>"
-      $(list_id).append(no_items)
-    }
+function checkIfArticlesSelectedAndAppend(el_id){
+  let items = el_id + " .item"
+  if ($(items).length === 0) {
+    let list_id = el_id
+    let no_items = "<div id=\"c4d_no_items_selected\" class=\"item white_text_black_bg\">No articles selected</div>"
+    $(list_id).append(no_items)
   }
+}
 
 })
