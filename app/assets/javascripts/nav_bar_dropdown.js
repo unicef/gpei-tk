1381b635b979ca1a647cc39a99c857fb0ebfe32a
eUstundag <smokecode@gmail.com>
2016-07-07 15:44:41 -0400

    removed unnecessary dropdown js

--- app/assets/javascripts/nav_bar_dropdown.js
+++ app/assets/javascripts/nav_bar_dropdown.js
@@ -1,25 +1,6 @@
 $(function(){
-  // $('#my_rhizome_dropdown')
-  // .mouseover(function() {
-  //   $('.ui.selection.dropdown').dropdown('show');
-  // })
-  // .mouseleave(function() {
-  //   $('.ui.selection.dropdown').dropdown('hide');
-  // });
   $('.ui.pointing.dropdown.link.item')
   .dropdown({
-    on: 'hover',
     action: 'nothing'
-  });
-
-
-  // $('#my_rhizome_dropdown')
-  // .hover(function() {
-  //   $('.ui.pointing.dropdown.top.right.link.item').dropdown('show');
-  // })
-  // .click(function() {
-  //   $('.ui.pointing.dropdown.link.item').dropdown('hide');
-  // });
-
-});
-
+  })
+})
