c3b6e4af566998693f00a0a6635baa8d210a4adb
eUstundag <smokecode@gmail.com>
2016-07-08 19:59:48 -0400

    user creation routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+  def new
+    if request.xhr?
+      responsible_offices = ResponsibleOffice.all
+      render json: { responsible_offices: responsible_offices, status: 'success' }
+    end
+  end
+
+  def create
+    @user = User.new(safe_user_params)
+    @user.role = Role.find_by(title: 'Member')
+    if @user.save
+      session[:uuid] = SecureRandom.base64
+      session[:id] = @user.id
+      current_time = Time.current
+      session[:expires_at] = current_time + 6.hours
+      session[:last_seen] = current_time
+      render :js => "window.location = '/'"
+    else
+      render json: { status: 403, errors: @user.errors.messages }
+    end
+  end
+
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+  end
+
+  def safe_user_params
+    params.require(:user).permit(:first_name, :last_name, :country, :responsible_office_id, :organization, :email, :password)
   end
 end
