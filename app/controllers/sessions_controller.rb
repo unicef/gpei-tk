724bb46afc3019eefbf5a421b1d4998a0f5743f9
eUstundag <smokecode@gmail.com>
2016-07-08 19:57:44 -0400

    sessions controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(email: params['user']['email'])
+    if @user.authenticate(params['user']['password'])
+      session[:uuid] = SecureRandom.base64
+      session[:id] = @user.id
+      current_time = Time.current
+      session[:expires_at] = current_time + 6.hours
+      session[:last_seen] = current_time
+      render :js => "window.location = '/'"
+    else
+      render json: { status: 'fail', error: 'Please re-enter your email or password and try again.' }
+    end
+  end
+
+  def destroy
+    session.clear
+    render :js => "window.location = '/'"
+  end
+end
