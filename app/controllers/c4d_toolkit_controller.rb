0efb283e74447addddace624da0bb733c46270b6
eUstundag <smokecode@gmail.com>
2016-07-11 16:06:30 -0400

    session verify for routes

--- app/controllers/c4d_toolkit_controller.rb
+++ app/controllers/c4d_toolkit_controller.rb
@@ -1,26 +1,30 @@
 class C4dToolkitController < ApplicationController
   def create
     if request.xhr?
-      user = current_user
-      article = C4dArticle.find_by(id: params['id'])
-      if user.c4d_toolkit.c4d_articles.find_by(id: article.id).nil?
-        user.c4d_toolkit.c4d_articles << article
-        render json: { status: "success", id: article.id, title: article.title }
-      else
-        render json: { status: "fail" }
+      if current_user
+        user = current_user
+        article = C4dArticle.find_by(id: params['id'])
+        if user.c4d_toolkit.c4d_articles.find_by(id: article.id).nil?
+          user.c4d_toolkit.c4d_articles << article
+          render json: { status: 200, id: article.id, title: article.title }
+        else
+          render json: { status: 403 }
+        end
       end
     end
   end
 
   def destroy
     if request.xhr?
-      user = current_user
-      article = user.c4d_toolkit.c4d_articles.find_by(id: params['id'])
-      if article
-        C4dToolkitC4dArticle.where(c4d_article_id: article.id, c4d_toolkit_id: user.c4d_toolkit.id).first.destroy
-        render json: { status: "success", id: params['id'] }
-      else
-        render json: { status: "fail" }
+      if current_user
+        user = current_user
+        article = user.c4d_toolkit.c4d_articles.find_by(id: params['id'])
+        if article
+          C4dToolkitC4dArticle.where(c4d_article_id: article.id, c4d_toolkit_id: user.c4d_toolkit.id).first.destroy
+          render json: { status: 200, id: params['id'] }
+        else
+          render json: { status: 403 }
+        end
       end
     end
   end
--- app/controllers/sop_checklist_controller.rb
+++ app/controllers/sop_checklist_controller.rb
@@ -1,26 +1,30 @@
 class SopChecklistController < ApplicationController
   def create
     if request.xhr?
-      user = current_user
-      article = SopArticle.find_by(id: params['id'])
-      if user.sop_checklist.sop_articles.find_by(title: article.title).nil?
-        user.sop_checklist.sop_articles << article
-        render json: { status: "success", id: article.id, article_title: article.title }
-      else
-        render json: { status: "fail" }
+      if current_user
+        user = current_user
+        article = SopArticle.find_by(id: params['id'])
+        if user.sop_checklist.sop_articles.find_by(title: article.title).nil?
+          user.sop_checklist.sop_articles << article
+          render json: { status: "success", id: article.id, article_title: article.title }
+        else
+          render json: { status: "fail" }
+        end
       end
     end
   end
 
   def destroy
     if request.xhr?
-      user = current_user
-      article = user.sop_checklist.sop_articles.find_by(id: params['id'])
-      if article
-        SopChecklistSopArticle.where(sop_article_id: article.id, sop_checklist_id: user.sop_checklist.id).first.destroy
-        render json: { status: "success", id: article.id, article_title: article.title }
-      else
-        render json: { status: "fail" }
+      if current_user
+        user = current_user
+        article = user.sop_checklist.sop_articles.find_by(id: params['id'])
+        if article
+          SopChecklistSopArticle.where(sop_article_id: article.id, sop_checklist_id: user.sop_checklist.id).first.destroy
+          render json: { status: "success", id: article.id, article_title: article.title }
+        else
+          render json: { status: "fail" }
+        end
       end
     end
   end
