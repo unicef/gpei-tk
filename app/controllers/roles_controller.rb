19a413380a7abd1eb670e20de4266a0a87e01229
eUstundag <smokecode@gmail.com>
2016-07-11 15:35:14 -0400

    route removed

--- app/controllers/roles_controller.rb
+++
@@ -1,8 +0,0 @@
-class RolesController < ApplicationController
-  def index
-    if request.xhr?
-      roles = Role.all.where("title != ?", 'root')
-      render json: { status: "success", roles: roles }
-    end
-  end
-end
