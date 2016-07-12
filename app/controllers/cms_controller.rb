8302e66a61a55e04c9c46c0b354540b7f503087a
eUstundag <smokecode@gmail.com>
2016-07-12 13:12:34 -0400

    fix server fail on user not logged in

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -1,5 +1,5 @@
 class CmsController < ApplicationController
   def index
-    redirect_to root_url unless current_user.is_admin? || current_user.is_editor?
+    redirect_to root_url unless current_user && (current_user.is_admin? || current_user.is_editor?)
   end
 end
