2f88a2ca6a823220d70003b9fc89d7b04a31e30a
eUstundag <smokecode@gmail.com>
2016-07-18 13:28:18 -0400

    remove template link controller

--- app/controllers/cms/template_links_controller.rb
+++
@@ -1,9 +0,0 @@
-class Cms::TemplateLinksController < ApplicationController
-  def index
-    template_links = TemplateLink.all
-    render json: { template_links: template_links, status: 200 }
-  end
-
-  def show
-  end
-end
