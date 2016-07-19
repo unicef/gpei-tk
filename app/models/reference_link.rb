02c91305fd7d7d40d6f12ef4b1e080ff13f56369
eUstundag <smokecode@gmail.com>
2016-07-19 14:16:35 -0400

    dynamic aws path generation for files

--- app/models/reference_link.rb
+++ app/models/reference_link.rb
@@ -1,4 +1,10 @@
 class ReferenceLink < ActiveRecord::Base
   belongs_to :reference_linkable, :polymorphic => true
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  has_attached_file :document,
+                    :path => 'reference_links/:language/:filename'
+  validates_attachment :document, content_type: { content_type: "application/pdf" }
+
+  Paperclip.interpolates :language { |attachment, style| attachment.instance.language }
 end
