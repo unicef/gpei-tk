14d92b6fd0147a19a4662645b41220477d5081d4
eUstundag <smokecode@gmail.com>
2016-07-19 14:15:24 -0400

    user side handling of reference links to aws

--- app/assets/javascripts/cms_assets.js
+++ app/assets/javascripts/cms_assets.js
@@ -3,48 +3,95 @@ $(() => {
     e.preventDefault()
   })
 
+  // $('#CMS_index_content').on('click', '#add_reference_link_input', e => {
+  //   e.preventDefault()
+  //   $(e.currentTarget.parentElement.parentElement).after(getReferenceLinkField())
+  //   return false
+  // })
+
   $('#CMS_references_link').click(e => {
     e.preventDefault()
-  })
+    $.ajax({
+      method: 'GET',
+      url: '/cms/reference_links/'
+    }).done(response => {
+      let reference_links = response.reference_links
+      $.ajax({
+        method: 'GET',
+        url: '/cms/users/'
+      }).done(response => {
+        $('#CMS_index_content').empty()
+        appendReferenceLinkHeader()
+        appendReferenceLinkRows(reference_links, response.users)
+      })
+    })
 
-  $('#CMS_templates_link').click(e => {
-    e.preventDefault()
   })
 
   $('#CMS_references_link_upload').click(e => {
     e.preventDefault()
+    let content = formForAssetsUpload()
+    $('#CMS_index_content').empty()
+    $('#CMS_index_content').append(content)
   })
-
-  $('#CMS_templates_link_upload').click(e => {
+  $('#CMS_index_content').on('submit', '#CMS_asset_upload_form', e => {
     e.preventDefault()
-  })
+    let formData = new FormData($(e.currentTarget)[0])
+    $.ajax({
+      method: 'POST',
+      cache: false,
+      contentType: false,
+      processData: false,
+      url: '/cms/reference_links/',
+      data: formData
+    }).done(response => {
 
-  $('#CMS_index_content').on('click', '#add_template_link_input', e => {
-    e.preventDefault()
-    $(e.currentTarget.parentElement.parentElement).after(getTemplateLinkField())
+    })
     return false
   })
-
-  $('#CMS_index_content').on('click', '#add_reference_link_input', e => {
-    e.preventDefault()
-    $(e.currentTarget.parentElement.parentElement).after(getReferenceLinkField())
-    return false
+  $('#CMS_index_content').on('change', '.reference_link_file', e => {
   })
 
-  function getTemplateLinkField() {
+  function getReferenceLinkField() {
+    // <i class="fa fa-plus" aria-hidden="true"></i>
     return (`
       <div class="field">
-        <label>Template Links<a id="add_template_link_input"  href=''><i class="fa fa-plus" aria-hidden="true"></i></a></label>
-        <input class="template_link_file" type="file" name="template_links" value="">
+        <label>Reference Link<a id="add_reference_link_input"  href=''></a></label>
+        <input class="reference_link_file" type="file" name="reference_link[document]" value="">
       </div>
     `)
   }
 
-  function getReferenceLinkField() {
+  function formForAssetsUpload() {
+    return (`
+      <form id="CMS_asset_upload_form" class="ui form CMS_c4d_article_form_div">
+        ${getLanguageDropdown()}
+        ${getReferenceLinkField()}
+        <button class="ui button" type="submit">Submit</button>
+      </form>
+      `)
+  }
+
+  function appendReferenceLinkHeader(){
+    $('#CMS_index_content').append('<table id="CMS_reference_link_table" class="ui celled table"></table>')
+    $('#CMS_reference_link_table').append('<thead><tr><th class="text-center"> Name </th><th class="text-center"> Updated </th><th class="text-center"> Created </th><th class="text-center"> Author </th></tr></thead>')
+  }
+
+  function appendReferenceLinkRows(reference_links, users){
+    _.forEach(reference_links, reference_link => {
+      let row = `<tr id="${reference_link.id}"><td>${reference_link.id}</td><td>${reference_link.file_name}</td><td>${moment(reference_link.updated_at, "YYYY-MM-DD").format("MMM DD, YYYY")}</td><td>${moment(reference_link.created_at, "YYYY-MM-DD").format("MMM DD, YYYY")}</td><td>${users[reference_link.author_id].first_name + ' ' + users[reference_link.author_id].last_name}</td></tr>`
+      $('#CMS_reference_link_table').append(row)
+    })
+  }
+  function getLanguageDropdown(){
     return (`
       <div class="field">
-        <label>Reference Links<a id="add_reference_link_input"  href=''><i class="fa fa-plus" aria-hidden="true"></i></a></label>
-        <input class="reference_link_file" type="file" name="reference_links" value="">
+        <label>Language</label>
+        <select name="reference_link[language]" class="ui dropdown cms_dropdown_select" required>
+          <option value="">Select Language</option>
+          <option value="en">English</option>
+          <option value="fr">French</option>
+        </select>
       </div>
     `)
   }
--- app/controllers/cms/reference_links_controller.rb
+++ app/controllers/cms/reference_links_controller.rb
@@ -4,6 +4,14 @@ class Cms::ReferenceLinksController < ApplicationController
     render json: { reference_links: reference_links, status: 200 }
   end
 
-  def show
+  def create
+    reference_link = ReferenceLink.new(author_id: current_user.id,
+                      document: params[:reference_link][:document],
+                      language: params[:reference_link][:language])
+    if reference_link.save
+      render json: { status: 200 }
+    else
+      render json: { status: 403 }
+    end
   end
 end
