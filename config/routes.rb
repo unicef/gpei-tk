7f52acfac215af0d45fa1f0323f350ac729e3b47
eUstundag <smokecode@gmail.com>
2016-07-20 18:27:23 -0400

    route for embedded image create

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
     patch '/sop_articles/publish/:id', to: 'sop_articles#publish', as: :sop_article_publish
     patch '/c4d_articles/publish/:id', to: 'c4d_articles#publish', as: :c4d_article_publish
     resources :reference_links, only: [:index, :create]
+    resources :embedded_images, only: [:create]
   end
 
   namespace :api, defaults: { format: :json } do
