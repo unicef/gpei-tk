8c58016a68aa76362183cf4de76009464565b5be
eUstundag <smokecode@gmail.com>
2016-07-20 18:26:22 -0400

    route for embedded image to s3

---
+++ app/controllers/cms/embedded_images_controller.rb
@@ -0,0 +1,13 @@
+class Cms::EmbeddedImagesController < ApplicationController
+  def create
+    embedded_image = EmbeddedImage.new(
+                      image: params[:embedded_image][:image],
+                      c4d_article_id: params[:embedded_image][:article])
+    embedded_image.url = embedded_image.image.url
+    if embedded_image.save
+      render json: { status: 200 }
+    else
+      render json: { status: 403 }
+    end
+  end
+end
