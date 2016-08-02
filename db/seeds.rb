63a4e68b8d2384b29508d755f258a231217513a3
eUstundag <smokecode@gmail.com>
2016-08-02 09:14:43 -0400

    manual ordering for tiles to reflect current production

--- db/seeds.rb
+++ db/seeds.rb
@@ -199,9 +199,6 @@ def createC4dArticle(row_elements, admin_user)
   c4d_article.published = true
   c4d_article.author_id = admin_user.id
   row_elements[-2].split("<dynamic-element name=")[1..-1].each do |field|
-    # if c4d_article.cms_title == "c4d_19"
-    #   binding.pry
-    # end
     field_data = field[field.index('><![CDATA[')+10..field.index(']]')-1]
     if !field.index("\"Sub-Category\"").nil? && field.index("\"Sub-Category\"") == 0
       c4d_article.c4d_subcategory_id = C4dSubcategory.find_by(title: field_data).id
@@ -382,4 +379,279 @@ article = SopArticle.where('lower(title) = ? AND sop_time_id = ?', "Implement St
 article.video_url = 'https://vimeo.com/136684822'
 videoPersistRaise(article)
 
+# delete duplicate articles #
+
+SopArticle.where('title ILIKE ?', 'Communicate Risk Assessment Through IHR').last.destroy
+SopArticle.where(content: "Finalize the six-month outbreak response plan document and make it available to all partners.").last.destroy
+SopArticle.where('title ILIKE ? AND sop_time_id = 2', 'Rapid Response Phase').last.destroy
+SopArticle.where('title ILIKE ?', 'Implement Strategic Communication Plan').last.destroy
+# FIX ARTICLE ORDERING #
+
+mysql_article_titles = [
+                        [1, 1, "Ensure Government Notification", 1],
+                        [1, 1, "Initiate Epidemiological & Social Investigation", 2],
+                        [1, 1, "Ensure Notification of GPEI's EOMG", 3],
+                        [1, 3, "Activate GPEI's RRT", 4],
+                        [1, 4, "Conduct Analysis Of AFP Databases", 5],
+                        [1, 4, "Assess Security And Access Situation", 6],
+                        [1, 3, "Assess Human Resource Capacity", 7],
+                        [1, 6, "Alert Supply Division", 8],
+                        [2, 2, "Conference Calls With GPEI Partners", 9],
+                        [2, 2, "Advocate Expedited Visas", 10],
+                        [2, 2, "Write to Health Minister", 11],
+                        [2, 2, "Convene Meeting Of Stakeholders", 12],
+                        [2, 1, "Finalize Epidemiological & Social Investigation", 13],
+                        [2, 2, "Communicate Risk Assessment Through IHR", 14],
+                        [2, 2, "Communicate With Donor Community And Media", 15],
+                        [2, 2, "Align to Conduct Interventions", 16],
+                        [2, 3, "Deploy RRT", 17],
+                        [2, 3, "Develop Initial Immunization Response", 18],
+                        [2, 3, "Implement Actions Required By PHEIC", 19],
+                        [2, 3, "Develop Outbreak Response Document", 20],
+                        [2, 3, "Establish Outbreak Task Forces", 21],
+                        [2, 3, "Initiate Enhanced Surveillance", 22],
+                        [2, 3, "Conduct Additional Intervention", 23],
+                        [2, 3, "Identify Human Resource Surge Capacity", 24],
+                        [2, 3, "Provide Updated Materials And Guidelines", 25],
+                        [2, 4, "Compile And Produce SITREP", 26],
+                        [2, 4, "Complete Situational Data Analysis", 27],
+                        [2, 5, "Share Polio Toolkit", 28],
+                        [2, 5, "Media Landscape Analysis", 29],
+                        [2, 5, "Identify Media Focal Person", 30],
+                        [2, 5, "Finalize Media Protocol", 31],
+                        [2, 5, "Press Brief/Media Release", 32],
+                        [2, 5, "Finalize C4D Messages", 33],
+                        [2, 5, "Receive And Review Media Releases", 34],
+                        [2, 1, "EOMG Outbreak Grading", 35],
+                        [2, 6, "Evaluate Administrative Capacity", 36],
+                        [2, 6, "Allocate Lump-Sum Funding", 37],
+                        [2, 6, "Order Vaccine and Initiate Transport", 38],
+                        [2, 7, "Grade Outbreak", 39],
+                        [2, 7, "Create On-Call Staff List", 40],
+                        [2, 7, "Initiate Full Investigation", 41],
+                        [2, 7, "Rapid Response Phase", 42],
+                        [2, 2, "Establish Outbreak Response Cell", 43],
+                        [2, 2, "Develop External Communication Guidance", 44],
+                        [3, 2, "Establish Weekly Stakeholder Meeting", 45],
+                        [3, 2, "Inform Goverments Outbreak Zone Countries", 46],
+                        [3, 2, "Develop Funding And Plan Advocacy", 47],
+                        [3, 3, "Deploy Surge Team", 48],
+                        [3, 3, "Brief And Deploy Surge Staff", 49],
+                        [3, 3, "Initial Immunization Response Campaigns", 50],
+                        [3, 3, "Establish Campaign Monitoring", 51],
+                        [3, 3, "Finalize Microplanning, Tools And Manuals", 52],
+                        [3, 3, "Implement Outbreak Response Plan", 53],
+                        [3, 4, "Establish Weekly SITREPs", 54],
+                        [3, 4, "Liase With In-Country Data Managers", 55],
+                        [3, 5, "Finalize C4D Community Engagement", 56],
+                        [3, 5, "Facilitate Social Mobilization", 57],
+                        [3, 5, "Develop Media Response Plan", 58],
+                        [3, 5, "Work With The Media", 59],
+                        [3, 5, "Support Partners' Communication Campaigns", 60],
+                        [3, 5, "Ensure Availability of IEC Materials", 61],
+                        [3, 5, "Begin Communication/IPC Training", 62],
+                        [3, 5, "Ensure Microplanning", 63],
+                        [3, 6, "Review and Release Budget", 64],
+                        [3, 6, "Assess Cold-Chain Capacity", 65],
+                        [3, 6, "Order For Additional Campaigns", 66],
+                        [3, 6, "Review Additional Support", 67],
+                        [3, 6, "Fill Vacant Positions", 68],
+                        [4, 2, "Conduct Weekly Stakeholder Meeting", 69],
+                        [4, 2, "Weekly Calls With GPEI Partners", 70],
+                        [4, 2, "Conduct Donor Meetings And Advocacy", 71],
+                        [4, 3, "Implement Outbreak Response Plan", 72],
+                        [4, 3, "Conduct SIAs", 73],
+                        [4, 3, "Maintain Enhanced Surveillance", 74],
+                        [4, 4, "Continue Weekly SITREPs", 75],
+                        [4, 4, "Ensure Data Completion And Transmission", 76],
+                        [4, 5, "Implement Strategic Communication Plan", 77],
+                        [4, 6, "Implement Six-Month Response Plan", 78],
+                        [4, 7, "Investigate Poliovirus Event", 79],
+                        [4, 7, "Surge Response Phase", 80],
+                        [4, 7, "Assess Response Activities", 81],
+                        [4, 7, "Update Outbreak Grade", 82]
+                      ]
+
+all_sop_articles = SopArticle.all
+fz = FuzzyMatch.new(mysql_article_titles, :read => 2, :find_best => true, :find_all => true)
+all_sop_articles.each do |current_article|
+  match_found = false
+  found_articles = fz.find(current_article.title)
+  if !found_articles.empty?
+    found_articles.each do |found_article|
+      if found_article[0] == current_article.sop_time_id &&
+         found_article[1] == current_article.sop_category_id
+
+        current_article.update(title: found_article[2], sop_time_id: found_article[0], sop_category_id: found_article[1], order_id: found_article[3])
+        match_found = true
+      end
+    end
+  end
+  if !match_found && !found_articles.empty?
+    current_article.update(title: found_articles[0][2], sop_time_id: found_articles[0][0], sop_category_id: found_articles[0][1], order_id: found_articles[0][3])
+  end
+  match_found
+end
+
+# fix typo #
+SopArticle.where('title ILIKE ? AND sop_time_id = 3', 'Inform goverments outbreak zone countries').last.update(title: 'Inform Governments Outbreak Zone Countries')
+
+# create missing articles
+article = SopArticle.create(
+  sop_time_id: 3,
+  sop_category_id: 3,
+  title: "Implement Outbreak Response Plan",
+  order_id: 53,
+  content: 'Finalize the six-month outbreak response plan document and make it available to all partners.',
+  responsible: 'RAPID RESPONSE TEAM AND SURGE TEAM, WITH REPURPOSED COUNTRY STAFF'.downcase,
+  responsible_office_id: 5,
+  published: true,
+  author_id: admin_user.id
+  )
+icon_title = SopIcon.where(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id).first.title
+SopIcon.create(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id, sop_article_id: article.id, title: icon_title)
+
+# gpei_id = SupportAffiliation.where(title: 'GPEI').first.id
+article = SopArticle.create(
+  sop_time_id: 4,
+  sop_category_id: 7,
+  title: "Investigate Poliovirus Event",
+  order_id: 79,
+  content: 'Upon receiving a report of a poliovirus event, the GPEI will support the country with technical guidance to investigate, assess and monitor the event. The poliovirus event should trigger putting the surveillance system on high alert to detect any signs of poliovirus transmission. A detailed investigation of the case and community and a travel history should urgently be conducted. The community should be searched for unreported cases, with visits to health-care facilities and other health-care providers, including traditional healers. Immunization response planning should begin in case the investigation and evaluation identify that the event is actually a polio outbreak.',
+  support: 'GPEI',
+  responsible_office_id: 3,
+  published: true,
+  author_id: admin_user.id
+  )
+icon_title = SopIcon.where(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id).first.title
+SopIcon.create(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id, sop_article_id: article.id, title: icon_title)
+
+article = SopArticle.create(
+  sop_time_id: 4,
+  sop_category_id: 7,
+  title: "Surge Response Phase",
+  order_id: 80,
+  content: 'Within three weeks of the outbreak notification, the GPEI makes available, consistent with the outbreak grade, a multidisciplinary and trained surge team and additional surge staff as needed and outlined in the outbreak response plan.',
+  responsible: 'GPEI',
+  responsible_office_id: 3,
+  published: true,
+  author_id: admin_user.id
+  )
+icon_title = SopIcon.where(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id).first.title
+SopIcon.create(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id, sop_article_id: article.id, title: icon_title)
+
+article = SopArticle.create(
+  sop_time_id: 4,
+  sop_category_id: 7,
+  title: "Assess Response Activities",
+  order_id: 81,
+  content: "After one month of confirmation of the index case, the GPEI will assess the response activities against established criteria and adapt the outbreak response plan's subsequent phases. The assessments will subsequently be conducted at three-month intervals until six months have passed without identification of the poliovirus. After six months without identification of the poliovirus, a final external assessment will be conducted, focusing on surveillance and activities to sustain the polio-free status in order to declare the end of the outbreak.",
+  responsible: 'GPEI',
+  responsible_office_id: 3,
+  published: true,
+  author_id: admin_user.id
+  )
+icon_title = SopIcon.where(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id).first.title
+SopIcon.create(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id, sop_article_id: article.id, title: icon_title)
+
+article = SopArticle.create(
+  sop_time_id: 4,
+  sop_category_id: 7,
+  title: "Update Outbreak Grade",
+  order_id: 82,
+  content: "The grade will be updated at least once every three months or whenever a significant change in the outbreak's evolution requires a re-evaluation of the assigned grade. Outbreaks will be declared closed if no transmission is detected for six months, with all subnational areas achieving sensitive surveillance after an external assessment following the end of the outbreak.",
+  responsible: 'GPEI',
+  responsible_office_id: 3,
+  published: true,
+  author_id: admin_user.id
+  )
+icon_title = SopIcon.where(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id).first.title
+SopIcon.create(sop_time_id: article.sop_time_id, sop_category_id: article.sop_category_id, sop_article_id: article.id, title: icon_title)
+
+# c4d corrections
+c4d_subcat_channel = C4dSubcategory.where(title: 'Channel Analysis').first
+C4dArticle.where(cms_title: 'c4d_19').first.update(c4d_subcategory_id: c4d_subcat_channel.id)
+
+mysql_c4d_articles = [
+  [ 1, 1, 'Situation Analysis', 1],
+  [ 1, 1, 'Prioritize', 2],
+  [ 1, 1, 'Epidemiological Risk Factors', 3],
+  [ 1, 1, 'Social Analysis', 4],
+  [ 1, 1, 'Writing an Effective Objective', 5],
+  [ 1, 2, 'Introduction', 6],
+  [ 1, 2, 'EPI and Campaign Data', 7],
+  [ 1, 2, 'Case Profiles', 8],
+  [ 1, 2, 'Social Data', 9],
+  [ 1, 3, 'Identify High Risk Groups', 10],
+  [ 2, 4, 'Introduction', 11],
+  [ 2, 4, 'Audience Analysis', 12],
+  [ 2, 4, 'Audience Segmentation Tool', 13],
+  [ 2, 5, 'Introduction', 14],
+  [ 2, 5, 'Barrier Analysis', 15],
+  [ 2, 5, 'Situational vs Attitudinal Barriers', 16],
+  [ 2, 6, 'Introduction', 17],
+  [ 2, 6, 'Message Themes', 18],
+  [ 2, 7, 'Media Selection', 19],
+  [ 2, 8, 'Introduction', 20],
+  [ 2, 8, 'Outbreak', 21],
+  [ 2, 8, 'Enduring Outbreak', 22],
+  [ 2, 8, 'Maintenance', 23],
+  [ 3, 9, 'Introduction', 24],
+  [ 3, 9, 'The Global Brand', 25],
+  [ 3, 9, 'Guiding Principles', 26],
+  [ 3, 9, 'The Journey to Vaccination', 27],
+  [ 3, 10, 'Introduction', 28],
+  [ 3, 10, 'Social Mobilization', 29],
+  [ 3, 10, 'Mass (Paid) Media', 30],
+  [ 3, 10, 'Owned Media', 31],
+  [ 3, 10, 'Earned Media', 32],
+  [ 3, 10, 'Advocacy Media', 33],
+  [ 3, 10, 'Multi-Media and SMS', 34],
+  [ 3, 11, 'Introduction', 35],
+  [ 3, 11, 'Monitor', 36],
+  [ 3, 11, 'Test', 37],
+  [ 3, 11, 'Evaluate', 38],
+  [ 4, 12, 'Global Commmunications Guide', 39],
+  [ 4, 12, 'Global Strategy Templates', 40],
+  [ 4, 12, 'Country Samples', 41],
+  [ 4, 13, 'Social Mobilization Curriculum', 42],
+  [ 4, 13, 'Vaccinator Training Kit', 43],
+  [ 4, 13, 'IEC Samples', 44],
+  [ 4, 14, 'Sample ToRs', 45],
+  [ 4, 14, 'Sample Job Descriptions', 46],
+  [ 4, 14, 'LTAs for C4D Support', 47],
+  [ 4, 15, 'Rapid Pro', 48],
+  [ 4, 15, 'Data', 49],
+  [ 4, 15, 'Design', 50],
+  [ 4, 15, 'mHealth', 51],
+  ]
+
+all_c4d_articles = C4dArticle.all
+fz = FuzzyMatch.new(mysql_c4d_articles, :read => 2, :find_best => true, :find_all => true)
+all_c4d_articles.each do |current_article|
+  match_found = false
+  found_articles = fz.find(current_article.title)
+  if !found_articles.empty?
+    found_articles.each do |found_article|
+      if found_article[0] == current_article.c4d_category_id &&
+         found_article[1] == current_article.c4d_subcategory_id
+
+        current_article.update(title: found_article[2], c4d_category_id: found_article[0], c4d_subcategory_id: found_article[1], order_id: found_article[3])
+        match_found = true
+      end
+    end
+  end
+  if !match_found && !found_articles.empty?
+    current_article.update(title: found_articles[0][2], c4d_category_id: found_articles[0][0], c4d_subcategory_id: found_articles[0][1], order_id: found_articles[0][3])
+  end
+  match_found
+end
+
+#c4d delete duplicates #
+C4dArticle.where(title: 'The Global Brand', c4d_category_id: 3, c4d_subcategory_id: 9, cms_title: 'c4d_54').last.destroy
+C4dArticle.where(title: 'Social Mobilization', c4d_category_id: 3, c4d_subcategory_id: 10, cms_title: 'c4d_53').last.destroy
+C4dArticle.where(title: 'Multi-Media and SMS', c4d_category_id: 3, c4d_subcategory_id: 10, cms_title: 'c4d_50').last.destroy
+C4dArticle.where(title: 'Social Mobilization Curriculum', c4d_category_id: 4, c4d_subcategory_id: 13, cms_title: 'c4d_42').last.destroy
+C4dArticle.where(title: 'Social Mobilization Curriculum', c4d_category_id: 4, c4d_subcategory_id: 13, cms_title: 'c4d_43').last.destroy
+C4dArticle.where(title: 'LTAs for C4D Support', c4d_category_id: 4, c4d_subcategory_id: 14, cms_title: 'c4d_52').last.destroy
 puts 'done!'
