9a6e255ce56c24108d35ade7888c242fd4eac4f7
eUstundag <smokecode@gmail.com>
2016-08-08 19:41:00 -0400

    handling for xhr c4d article show

--- app/controllers/c4d_articles_controller.rb
+++ app/controllers/c4d_articles_controller.rb
@@ -4,14 +4,35 @@ class C4dArticlesController < ApplicationController
   end
 
   def show
-    @user = current_user
-    @img_name = 'C4D'
-    if params['id'].nil?
-      @c4d_article = C4dArticle.where(title: params['title']).first
+    if request.xhr?
+      if params['id'].nil?
+        c4d_article = C4dArticle.where(title: params['title']).first
+      else
+        c4d_article = C4dArticle.where(id: params['id']).first
+      end
+      c4d_categories = C4dCategory.all
+      c4d_related_topics = C4dArticle.where("c4d_subcategory_id = ? AND id != ?", c4d_article.c4d_subcategory_id, c4d_article.id)
+      toolkit_articles = current_user.c4d_toolkit.c4d_articles if current_user
+      c4d_subcategories = C4dSubcategory.all
+      reference_links = c4d_article.reference_links
+      render json: { status: 200,
+                     article: c4d_article,
+                     c4d_categories: c4d_categories,
+                     c4d_subcategories: c4d_subcategories,
+                     c4d_related_topics: c4d_related_topics,
+                     current_user: current_user,
+                     toolkit_articles: toolkit_articles,
+                     reference_links: reference_links }
     else
-      @c4d_article = C4dArticle.where(id: params['id']).first
+      @user = current_user
+      @img_name = 'C4D'
+      if params['id'].nil?
+        @c4d_article = C4dArticle.where(title: params['title']).first
+      else
+        @c4d_article = C4dArticle.where(id: params['id']).first
+      end
+      @c4d_categories = C4dCategory.all
+      @c4d_related_topics = C4dArticle.where("c4d_subcategory_id = ? AND id != ?", @c4d_article.c4d_subcategory_id, @c4d_article.id)
     end
-    @c4d_categories = C4dCategory.all
-    @c4d_related_topics = C4dArticle.where("c4d_subcategory_id = ? AND id != ?", @c4d_article.c4d_subcategory_id, @c4d_article.id)
   end
 end
