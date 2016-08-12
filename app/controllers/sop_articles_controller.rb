d51a3068a20332c050196dc92a2356d3250a91d1
eUstundag <smokecode@gmail.com>
2016-08-12 12:11:29 -0400

    sop article route for xhr

--- app/controllers/sop_articles_controller.rb
+++ app/controllers/sop_articles_controller.rb
@@ -4,19 +4,40 @@ class SopArticlesController < ApplicationController
   end
 
   def show
-    @user = current_user
-    @img_name = 'SOP'
-    if params['id'].nil?
-      @sop_article = SopArticle.find_by(title: params['title'])
+    if request.xhr?
+      if params['id'].nil?
+        sop_article = SopArticle.where(title: params['title']).first
+      else
+        sop_article = SopArticle.where(id: params['id']).first
+      end
+      sop_categories = SopCategory.all
+      sop_related_topics = SopArticle.where("sop_category_id = ? AND id != ?", sop_article.sop_category_id, sop_article.id)
+      checklist_articles = current_user.sop_checklist.sop_articles if current_user
+      sop_times = SopTime.all
+      reference_links = sop_article.reference_links
+      render json: { status: 200,
+                     article: sop_article,
+                     sop_categories: sop_categories,
+                     sop_times: sop_times,
+                     sop_related_topics: sop_related_topics,
+                     current_user: current_user,
+                     checklist_articles: checklist_articles,
+                     reference_links: reference_links }
     else
-      @sop_article = SopArticle.find_by(id: params['id'])
+      @user = current_user
+      @img_name = 'SOP'
+      if params['id'].nil?
+        @sop_article = SopArticle.find_by(title: params['title'])
+      else
+        @sop_article = SopArticle.find_by(id: params['id'])
+      end
+      last_article = SopArticle.order(:id).last
+      if !last_article.nil?
+        @sop_article_last_index = last_article.id
+      else
+        @sop_article_last_index = 0
+      end
+      @sop_related_topics = SopArticle.where("sop_category_id = ? AND id != ?", @sop_article.sop_category_id, @sop_article.id)
     end
-    last_article = SopArticle.order(:id).last
-    if !last_article.nil?
-      @sop_article_last_index = last_article.id
-    else
-      @sop_article_last_index = 0
-    end
-    @sop_related_topics = SopArticle.where("sop_category_id = ? AND id != ?", @sop_article.sop_category_id, @sop_article.id)
   end
 end
