1f6f4e8f486e351c430b3479b431e4cba9135f42
eUstundag <smokecode@gmail.com>
2016-09-06 11:06:20 -0400

    style change and pdf thumbnail attempt

--- app/models/reference_link.rb
+++ app/models/reference_link.rb
@@ -3,7 +3,8 @@ class ReferenceLink < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   has_attached_file :document,
-                    :path => 'reference_links/:language/:filename'
+                    :path => 'reference_links/:language/:filename',
+                    :styles => { :thumb => ["200x200>", :png] }
   validates_attachment :document, content_type: { content_type: "application/pdf" }
 
   Paperclip.interpolates :language do |attachment, style|
