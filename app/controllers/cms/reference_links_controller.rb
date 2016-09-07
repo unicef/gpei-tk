0861e225cd7131d29c51af026a41f5a3c578d27b
eUstundag <smokecode@gmail.com>
2016-09-07 14:15:17 -0400

    xhr request check

--- app/controllers/cms/reference_links_controller.rb
+++ app/controllers/cms/reference_links_controller.rb
@@ -5,14 +5,16 @@ class Cms::ReferenceLinksController < ApplicationController
   end
 
   def create
-    reference_link = ReferenceLink.new(author_id: current_user.id,
-                      document: params[:reference_link][:document],
-                      language: params[:reference_link][:language])
-    reference_link.url = reference_link.document.url
-    if reference_link.save
-      render json: { status: 200 }
-    else
-      render json: { status: 403 }
+    if request.xhr?
+      reference_link = ReferenceLink.new(author_id: current_user.id,
+                        document: params[:reference_link][:document],
+                        language: params[:reference_link][:language])
+      # reference_link.absolute_url = reference_link.document.url
+      if reference_link.save
+        render json: { status: 200 }
+      else
+        render json: { status: 403 }
+      end
     end
   end
 end
