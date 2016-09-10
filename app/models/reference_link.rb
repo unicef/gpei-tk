0374e50dcf513a7cc074476c1522e67bb26dfa38
eUstundag <smokecode@gmail.com>
2016-09-10 12:30:47 -0400

    remove thumbnail setting till this is fixed

--- app/models/reference_link.rb
+++ app/models/reference_link.rb
@@ -3,8 +3,7 @@ class ReferenceLink < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   has_attached_file :document,
-                    :path => 'reference_links/:language/:filename',
-                    styles: { medium: "300x300>", thumb: "100x100>" }
+                    :path => 'reference_links/:language/:filename'
 
   validates_attachment :document, content_type: { content_type: "application/pdf" }
 
