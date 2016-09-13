e868e9952d9c0c671ea77d8a6fefe6801b263c53
eUstundag <smokecode@gmail.com>
2016-09-13 09:20:01 -0400

    include mp3 for reference link(temporary)

--- app/models/reference_link.rb
+++ app/models/reference_link.rb
@@ -5,7 +5,9 @@ class ReferenceLink < ActiveRecord::Base
   has_attached_file :document,
                     :path => 'reference_links/:language/:filename'
 
-  validates_attachment :document, content_type: { content_type: ["application/pdf", "application/vnd.openxmlformats-officedocument.presentationml.slideshow"] }
+  validates_attachment :document, content_type: { content_type: ["application/pdf",
+                                                                "application/vnd.openxmlformats-officedocument.presentationml.slideshow",
+                                                                "audio/mp3"] }
 
   Paperclip.interpolates :language do |attachment, style|
     attachment.instance.language
