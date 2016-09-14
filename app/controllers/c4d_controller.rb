ead26733bf0481e2f3d1a6e3dbb879182e287725
eUstundag <smokecode@gmail.com>
2016-09-14 11:48:00 -0400

    fix color logic

--- app/controllers/c4d_controller.rb
+++ app/controllers/c4d_controller.rb
@@ -47,28 +47,28 @@ class C4dController < ApplicationController
   def get_c4d_article_color_array category
     colors = { 'Understand':
               { 'BehaviouralGoal': ['#4B5C16', '#586E12', '#6F8D0E', '#77980F', '#8DAC2B'],
-                'UsingEvidence': [nil, nil, nil, nil, nil, '#60791C', '#749115', '#80A30A', '#98B90B'],
-                'IDHighRiskGroups': [nil, nil, nil, nil, nil, nil, nil, nil, nil, '#81A228']
+                'UsingEvidence': ['#60791C', '#749115', '#80A30A', '#98B90B'],
+                'IDHighRiskGroups': ['#81A228']
               },
                 'Plan':
               { 'Segmentation': ['#142964', '#173283', '#1E40A5'],
-                'Barriers': [nil, nil, nil, '#192C93', '#1F36A8', '#2742CB'],
-                'Messaging': [nil, nil, nil, nil, nil, nil, '#215CAF', '#296ECE'],
-                'ChannelAnalysis': [nil, nil, nil, nil, nil, nil, nil, nil, '#4799DD'],
-                'IDYourScenario': [nil, nil, nil, nil, nil, nil, nil, nil, nil, '#6BA6D6', '#7CB5DE', '#85C0EA', '#93C7E8']
+                'Barriers': ['#192C93', '#1F36A8', '#2742CB'],
+                'Messaging': ['#215CAF', '#296ECE'],
+                'ChannelAnalysis': ['#4799DD'],
+                'IDYourScenario': ['#6BA6D6', '#7CB5DE', '#85C0EA', '#93C7E8']
               },
                 'Act':
               {
                 'TheGlobalStrategy': ['#0A462F', '#0E6040', '#14714C', '#17885B'],
-                'IntegratedAction': [nil, nil, nil, nil, '#085D40', '#0C6949', '#127653', '#16805B', '#1E8E67', '#21956D', '#28A278'],
-                'MonitorEvaluate': [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, '#5FA870', '#69B57B', '#7CC98E', '#579866'],
+                'IntegratedAction': ['#085D40', '#0C6949', '#127653', '#16805B', '#1E8E67', '#21956D', '#28A278'],
+                'MonitorEvaluate': ['#579866', '#5FA870', '#69B57B', '#7CC98E'],
               },
               'Tools':
               {
                 'MassMediaIEC': ['#175067', '#1F6480', '#2B7896'],
-                'Training': [nil, nil, nil, '#06868D', '#11979F', '#30AFB7'],
-                'ManagementTools': [nil, nil, nil, nil, nil, nil, '#29A8A8', '#2CB4B4', '#4BC6C6'],
-                'Innovations': [nil, nil, nil, nil, nil, nil, nil, nil, nil, '#4DAFAF', '#57B7B7', '#62C4C4', '#6ECCCC']
+                'Training': ['#06868D', '#11979F', '#30AFB7'],
+                'ManagementTools': ['#29A8A8', '#2CB4B4', '#4BC6C6'],
+                'Innovations': ['#4DAFAF', '#57B7B7', '#62C4C4', '#6ECCCC']
               }
             }
   end
