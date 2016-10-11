674a6373ca27a8b0bcd5d64303ef64439b9eaec9
eUstundag <smokecode@gmail.com>
2016-10-11 10:49:29 -0400

    change mail settings from aws to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
     :authentication       => :plain,
     :enable_starttls_auto => true
   }
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.delivery_method = :smtp
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
