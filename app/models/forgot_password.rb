55a99c61ab1c5efba0b5db0554496f5c1882445e
eUstundag <smokecode@gmail.com>
2016-10-11 15:37:49 -0400

    fix expired logic

--- app/models/forgot_password.rb
+++ app/models/forgot_password.rb
@@ -7,8 +7,8 @@ class ForgotPassword < ActiveRecord::Base
     if (self.created_at + 4.hours).past?
       self.update(expired: true)
       errors.add(:expired, 'forgotten password link has expired')
-      return false
+      return true
     end
-    true
+    false
   end
 end
