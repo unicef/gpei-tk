6b99e480017974cc8a6cf28485fd74a68ac59ae6
eUstundag <smokecode@gmail.com>
2016-10-21 02:54:01 -0400

    model method added to get category for reference link join table

--- app/models/reference_link_article.rb
+++ app/models/reference_link_article.rb
@@ -1,4 +1,14 @@
 class ReferenceLinkArticle < ActiveRecord::Base
   belongs_to :reference_linkable, :polymorphic => true
   belongs_to :reference_link
+
+  def category
+    if self.reference_linkable.has_attribute?(:sop_category_id)
+      self.reference_linkable.sop_category.title
+    elsif self.reference_linkable.has_attribute?(:c4d_category_id)
+      self.reference_linkable.c4d_category.title
+    else
+      nil
+    end
+  end
 end
