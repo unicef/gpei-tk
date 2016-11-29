06fa5663516959b332194b770588f3480a01db62
eUstundag <smokecode@gmail.com>
2016-11-29 12:25:59 -0500

    basic index and search route for library

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -2,4 +2,17 @@ class LibraryController < ApplicationController
   def index
 
   end
+
+  def search
+    ReferenceLink.search(safe_search_params)
+    SopArticle.search(safe_search_params)
+    C4dArticle.search(safe_search_params)
+    render json: { status: 200 }
+  end
+
+  private
+
+  def safe_search_params
+    params.require(:search).permit(:text)
+  end
 end
