724e17b39bebf86ff27428b6b0b8d0c4f478d7af
eUstundag <smokecode@gmail.com>
2016-12-06 16:51:11 -0500

    revert to before action due to url bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
-  after_action :allow_iframe
+  before_action :allow_iframe
   helper_method :current_user, :latest_notification
   protect_from_forgery with: :exception
 
