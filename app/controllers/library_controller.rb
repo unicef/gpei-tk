ee35cd2e2973cc178ea5afe8f7c51e4570f15ffd
eUstundag <smokecode@gmail.com>
2016-12-13 16:33:58 -0500

    search returns needed references

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -4,30 +4,25 @@ class LibraryController < ApplicationController
   end
 
   def referenceSearch
-    references = PgSearch.multisearch(safe_search_params[:query])
-    references.to_a.map! do |obj|
-      if obj.searchable_type == 'ReferenceLink'
-        if !ReferenceLinkArticle.where(reference_link_id: obj.searchable.id).empty?
-          return obj.searchable_type, obj.searchable
-        end
-      elsif obj.searchable_type == 'ReferenceMp3'
-        if !ReferenceMp3Article.where(reference_mp3_id: obj.searchable.id).empty?
-          return [obj.searchable_type, obj.searchable]
-        end
-      elsif obj.searchable_type == 'ReferencePptx'
-        if !ReferencePptxArticle.where(reference_pptx_id: obj.searchable.id).empty?
-          return [obj.searchable_type, obj.searchable]
-        end
+    reference_links = ReferenceLink.search_refs(params[:search][:query])
+    reference_links.collect! do |record|
+      binding.pry
+      if record.utilized?
+        { type: record.class.model_name.human, reference: record, isSop: record.isSOP?, isC4D: record.isC4D? }
       end
-      return nil
     end
-
-    render json: { status: 200, references: references }
-  end
-
-  private
-
-  def safe_search_params
-    params.require(:search).permit(:query)
+    reference_mp3s = ReferenceMp3.search_refs(params[:search][:query])
+    reference_mp3s.collect! do |record|
+      if record.utilized?
+        { type: record.class.model_name.human, reference: record, isSop: record.isSOP?, isC4D: record.isC4D? }
+      end
+    end
+    reference_pptxes = ReferencePptx.search_refs(params[:search][:query])
+    reference_pptxes.collect! do |record|
+      if record.utilized?
+        { type: record.class.model_name.human, reference: record, isSop: record.isSOP?, isC4D: record.isC4D? }
+      end
+    end
+    render json: { status: 200, references: (reference_links + reference_mp3s + reference_pptxes).compact }
   end
 end
