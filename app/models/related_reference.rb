3f55a98215dc12ffaed232007b8a4c6a720aa4f3
eUstundag <smokecode@gmail.com>
2016-12-15 13:34:39 -0500

    removed legacy method

--- app/models/related_reference.rb
+++ app/models/related_reference.rb
@@ -1,13 +1,3 @@
 class RelatedReference < ActiveRecord::Base
   belongs_to :reference_links
-
-  def category
-    if self.relatable_reference.has_attribute?(:sop_category_id)
-      self.relatable_reference.sop_category.title
-    elsif self.relatable_reference.has_attribute?(:c4d_category_id)
-      self.relatable_reference.c4d_category.title
-    else
-      nil
-    end
-  end
 end
