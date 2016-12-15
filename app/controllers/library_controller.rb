02a66559de803b6635c1017bdcd6da9b549a23d0
eUstundag <smokecode@gmail.com>
2016-12-15 00:04:26 -0500

    library ref search route updated to indicate c4d or sop for references and faster database queries

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -4,24 +4,20 @@ class LibraryController < ApplicationController
   end
 
   def referenceSearch
-    reference_links = ReferenceLink.search_refs(params[:search][:query])
-    reference_links.collect! do |record|
-      binding.pry
-      if record.utilized?
-        { type: record.class.model_name.human, reference: record, isSop: record.isSOP?, isC4D: record.isC4D? }
-      end
+    reference_links = ReferenceLink.joins(:reference_link_articles).search_refs(params[:search][:query]).map do |ref|
+      isSOP = true if ReferenceLinkArticle.where(reference_link_id: ref.id, reference_linkable_type: 'SopArticle').empty?
+      isC4D = true if ReferenceLinkArticle.where(reference_link_id: ref.id, reference_linkable_type: 'C4dArticle').empty?
+      { type: ref.class.model_name.human, reference: ref, isSOP: isSOP , isC4D: isC4D }
     end
-    reference_mp3s = ReferenceMp3.search_refs(params[:search][:query])
-    reference_mp3s.collect! do |record|
-      if record.utilized?
-        { type: record.class.model_name.human, reference: record, isSop: record.isSOP?, isC4D: record.isC4D? }
-      end
+    reference_mp3s = ReferenceMp3.joins(:reference_mp3_articles).search_refs(params[:search][:query]).map do |ref|
+      isSOP = true if ReferenceMp3Article.where(reference_mp3_id: ref.id, reference_mp3able_type: 'SopArticle').empty?
+      isC4D = true if ReferenceMp3Article.where(reference_mp3_id: ref.id, reference_mp3able_type: 'C4dArticle').empty?
+      { type: ref.class.model_name.human, reference: ref, isSOP: isSOP , isC4D: isC4D }
     end
-    reference_pptxes = ReferencePptx.search_refs(params[:search][:query])
-    reference_pptxes.collect! do |record|
-      if record.utilized?
-        { type: record.class.model_name.human, reference: record, isSop: record.isSOP?, isC4D: record.isC4D? }
-      end
+    reference_pptxes = ReferencePptx.joins(:reference_pptx_articles).search_refs(params[:search][:query]).map do |ref|
+      isSOP = true if ReferencePptxArticle.where(reference_pptx_id: ref.id, reference_pptxable_type: 'SopArticle').empty?
+      isC4D = true if ReferencePptxArticle.where(reference_pptx_id: ref.id, reference_pptxable_type: 'C4dArticle').empty?
+      { type: ref.class.model_name.human, reference: ref, isSOP: isSOP , isC4D: isC4D }
     end
     render json: { status: 200, references: (reference_links + reference_mp3s + reference_pptxes).compact }
   end
