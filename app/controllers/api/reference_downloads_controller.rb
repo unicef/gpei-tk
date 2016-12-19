7c19a5d783a676bdb1c86deb4c2f6492f27eafdc
eUstundag <smokecode@gmail.com>
2016-12-19 16:11:13 -0500

    user relation fixed on downloads

--- app/controllers/api/reference_downloads_controller.rb
+++ app/controllers/api/reference_downloads_controller.rb
@@ -1,9 +1,11 @@
 class Api::ReferenceDownloadsController < ApplicationController
   def create
     user = current_user || User.find_by(email: 'guest_not_signed_in@unicef.org')
-    download = Download.create(author: current_user)
+    download = Download.create(author: user)
     reference = ReferenceLink.find_by(id: params[:id])
-    reference_download = ReferenceDownload.create(download_id: download.id, reference_downloadable_id: reference.id, reference_downloadable_type: reference.class.model_name.name)
+    reference_download = ReferenceDownload.create(download_id: download.id,
+                                                  reference_downloadable_id: reference.id,
+                                                  reference_downloadable_type: reference.class.model_name.name)
     render json: { status: 200 }
   end
 end
