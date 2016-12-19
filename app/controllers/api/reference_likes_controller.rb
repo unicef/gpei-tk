0da95dc34139c23aa5bdbe7c04186b4da8b97b67
eUstundag <smokecode@gmail.com>
2016-12-19 15:38:46 -0500

    allow guest likes unlimited

--- app/controllers/api/reference_likes_controller.rb
+++ app/controllers/api/reference_likes_controller.rb
@@ -1,11 +1,11 @@
 class Api::ReferenceLikesController < ApplicationController
   def create
     user = current_user
+    reference = ReferenceLink.find_by(id: params[:id])
     if user
-      like_ids = Like.where(author_id: current_user).pluck(:id)
-      reference = ReferenceLink.find_by(id: params[:id])
+      like_ids = Like.where(author_id: user).pluck(:id)
       if like_ids.empty?
-        like = Like.create(author: current_user)
+        like = Like.create(author: user)
         reference_like = ReferenceLike.create(like_id: like.id,
                                               reference_likeable_id: reference.id,
                                               reference_likeable_type: reference.class.model_name.name)
@@ -14,7 +14,7 @@ class Api::ReferenceLikesController < ApplicationController
                                               reference_likeable_id: reference.id,
                                               reference_likeable_type: reference.class.model_name.name)
         if reference_likes.empty?
-          like = Like.create(author: current_user)
+          like = Like.create(author: user)
           reference_like = ReferenceLike.create(like_id: like.id,
                                                 reference_likeable_id: reference.id,
                                                 reference_likeable_type: reference.class.model_name.name)
@@ -22,7 +22,12 @@ class Api::ReferenceLikesController < ApplicationController
       end
       render json: { status: 200 }
     else
-      render json: { status: 403 }
+      user = User.find_by(email: 'guest_not_signed_in@unicef.org')
+      like = Like.create(author: user)
+      reference_like = ReferenceLike.create(like_id: like.id,
+                                            reference_likeable_id: reference.id,
+                                            reference_likeable_type: reference.class.model_name.name)
+      render json: { status: 200 }
     end
   end
 end
