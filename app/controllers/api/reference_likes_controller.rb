75513c96f44d61860422f9529dbef7327fb5ba4e
eUstundag <smokecode@gmail.com>
2016-12-22 13:17:23 -0500

    clean up logic, some DRY, add error route

--- app/controllers/api/reference_likes_controller.rb
+++ app/controllers/api/reference_likes_controller.rb
@@ -2,32 +2,37 @@ class Api::ReferenceLikesController < ApplicationController
   def create
     user = current_user
     reference = ReferenceLink.find_by(id: params[:id])
-    if user
-      like_ids = Like.where(author_id: user).pluck(:id)
-      if like_ids.empty?
-        like = Like.create(author: user)
-        reference_like = ReferenceLike.create(like_id: like.id,
-                                              reference_likeable_id: reference.id,
-                                              reference_likeable_type: reference.class.model_name.name)
-      else
-        reference_likes = ReferenceLike.where(like_id: like_ids,
-                                              reference_likeable_id: reference.id,
-                                              reference_likeable_type: reference.class.model_name.name)
-        if reference_likes.empty?
+    if reference
+      if user
+        like_ids = Like.where(author_id: user).pluck(:id)
+        if like_ids.empty?
           like = Like.create(author: user)
           reference_like = ReferenceLike.create(like_id: like.id,
                                                 reference_likeable_id: reference.id,
                                                 reference_likeable_type: reference.class.model_name.name)
-         end
+        else
+          reference_likes = ReferenceLike.where(like_id: like_ids,
+                                                reference_likeable_id: reference.id,
+                                                reference_likeable_type: reference.class.model_name.name)
+          if reference_likes.empty?
+            like = Like.create(author: user)
+            reference_like = ReferenceLike.create(like_id: like.id,
+                                                  reference_likeable_id: reference.id,
+                                                  reference_likeable_type: reference.class.model_name.name)
+           end
+        end
+      else
+        user = User.find_by(email: 'guest_not_signed_in@unicef.org')
+        like = Like.create(author: user)
+        reference_like = ReferenceLike.create(like_id: like.id,
+                                              reference_likeable_id: reference.id,
+                                              reference_likeable_type: reference.class.model_name.name)
       end
-      render json: { status: 200 }
+      reference_like_count = ReferenceLike.where(reference_likeable_id: reference.id,
+                                                 reference_likeable_type: reference.class.model_name.name).count
+      render json: { status: 200, like_count: reference_like_count }
     else
-      user = User.find_by(email: 'guest_not_signed_in@unicef.org')
-      like = Like.create(author: user)
-      reference_like = ReferenceLike.create(like_id: like.id,
-                                            reference_likeable_id: reference.id,
-                                            reference_likeable_type: reference.class.model_name.name)
-      render json: { status: 200 }
+      render json: { status: 403 }
     end
   end
 end
