d8a77c3c5c4a716eb6300468a5c10cd4ae8c7eb5
eUstundag <smokecode@gmail.com>
2017-01-04 13:50:33 -0500

    redirect to home page for navigation route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
   end
 
   def navigation
+    redirect_to '/' #remove when SOP returns to the fold.
     @display = 'block'
     @nav_bar_offset = 'col-md-offset-7'
   end
