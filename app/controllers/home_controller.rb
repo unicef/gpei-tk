1747ff215b8ea743d73ca5bdc2dcbbaf6a4cada1
eUstundag <smokecode@gmail.com>
2017-01-03 19:50:34 -0500

    set var to render block or not

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @display = 'none'
     @nav_bar_offset = 'col-md-offset-7'
   end
 
   def navigation
+    @display = 'block'
     @nav_bar_offset = 'col-md-offset-7'
   end
 end
