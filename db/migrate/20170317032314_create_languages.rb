2842171873571eabc66614c9d2dc47bc64d6a628
eUstundag <smokecode@gmail.com>
2017-03-16 23:25:47 -0400

    languages migration and join table

---
+++ db/migrate/20170317032314_create_languages.rb
@@ -0,0 +1,13 @@
+class CreateLanguages < ActiveRecord::Migration
+  def change
+    create_table :languages do |t|
+      t.text :title
+      t.timestamps null: false
+    end
+    create_table :language_references do |t|
+      t.integer :language_id
+      t.references :reference_languageable, polymorphic: true
+      t.timestamps null: false
+    end
+  end
+end
