ad367ccad7d59cc99de34a42e075497dae35c67c
eUstundag <smokecode@gmail.com>
2017-03-16 23:25:04 -0400

    places migration and join table

---
+++ db/migrate/20170317032302_create_places.rb
@@ -0,0 +1,13 @@
+class CreatePlaces < ActiveRecord::Migration
+  def change
+    create_table :places do |t|
+      t.text :title
+      t.timestamps null: false
+    end
+    create_table :place_references do |t|
+      t.integer :place_id
+      t.references :reference_placeable, polymorphic: true
+      t.timestamps null: false
+    end
+  end
+end
