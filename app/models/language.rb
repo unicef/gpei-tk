4cf81fcdcb144e1e58b213aec812b54a19accf36
eUstundag <smokecode@gmail.com>
2017-03-20 14:28:26 -0400

    null validation for title

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,5 @@
 class Language < ActiveRecord::Base
   belongs_to :language_references
   validates_uniqueness_of :title
+  validates_presence_of :title
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ActiveRecord::Base
   belongs_to :place_references
   validates_uniqueness_of :title
+  validates_presence_of :title
 end
