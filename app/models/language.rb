74796ac4cb454bb2cefb2795f504a55d423d9a1f
eUstundag <smokecode@gmail.com>
2017-03-24 11:22:45 -0400

    remove presence validation till places issue resolved

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,4 @@
 class Language < ActiveRecord::Base
   belongs_to :language_references
   validates_uniqueness_of :title
-  validates_presence_of :title
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,4 @@
 class Place < ActiveRecord::Base
   belongs_to :place_references
   validates_uniqueness_of :title
-  validates_presence_of :title
 end
