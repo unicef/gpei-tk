9951ee4efd296627e464016a1eab69cb42b5ccd0
eUstundag <smokecode@gmail.com>
2017-03-27 14:07:59 -0400

    schema for current database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317032314) do
+ActiveRecord::Schema.define(version: 20170324152655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -203,7 +203,7 @@ ActiveRecord::Schema.define(version: 20170317032314) do
     t.string   "description"
     t.string   "title"
     t.string   "document_language"
-    t.string   "places"
+    t.string   "countries"
     t.integer  "download_count"
     t.integer  "like_count"
   end
